c2f32c6a9761a67bafd073f031d0438e5e0f6002
Andre Duffeck <duffeck@kde.org>
2007-11-14 06:38:21 +0000

    fix typo
    svn path=/trunk/extragear/plasma/; revision=736413

--- dataengines/twitter/twitterengine.cpp
+++ dataengines/twitter/twitterengine.cpp
@@ -1,6 +1,6 @@
 /*
  *   Copyright (C) 2007 Trever Fischer <wm161@wm161.net>
- *   Copyright (C) 2007 André Duffeck <duffekc@kde.org>
+ *   Copyright (C) 2007 André Duffeck <duffeck@kde.org>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License version 2 as
