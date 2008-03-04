eed8d7ca91cc876273df04cf5a947fac917bf0de
Ivan Čukić <ivan.cukic@kde.org>
2008-03-04 22:47:21 +0000

    Added TODO/NOTE about renaming the macro in the event of including Puck into Plasma
    svn path=/trunk/extragear/plasma/; revision=782371

--- applets/lancelot/cmake/PlasmaUIMacros.cmake
+++ applets/lancelot/cmake/PlasmaUIMacros.cmake
@@ -2,6 +2,10 @@ set (KDE_PLASMA_UIC_EXECUTABLE ${CMAKE_SOURCE_DIR}/applets/lancelot/tools/puck/p
 
 message("Plasma UI Compiler (Puck) is located at: " ${KDE_PLASMA_UIC_EXECUTABLE})
 
+# TODO: Change the name of the KDE4_ADD_PLASMA_UI_FILES macro \
+# so that it complies with CMake style guides before (if) it  \
+# becomes a standard part of Plasma
+
 #usage: KDE4_ADD_PLASMA_UI_FILES(foo_SRCS ${ui_files})
 macro (KDE4_ADD_PLASMA_UI_FILES _sources )
    foreach (_current_FILE ${ARGN})
@@ -9,7 +13,7 @@ macro (KDE4_ADD_PLASMA_UI_FILES _sources )
       get_filename_component(_tmp_FILE ${_current_FILE} ABSOLUTE)
       get_filename_component(_basename ${_tmp_FILE} NAME_WE)
       set(_header ${CMAKE_CURRENT_BINARY_DIR}/ui_${_basename}.h)
-      
+
       message(${KDE_PLASMA_UIC_EXECUTABLE} "\n  " ${_tmp_FILE} "\n  " ${_header})
 
       add_custom_command(OUTPUT ${_header}
@@ -19,9 +23,9 @@ macro (KDE4_ADD_PLASMA_UI_FILES _sources )
 
          DEPENDS ${_current_FILE}
       )
-      
+
       list(APPEND ${_sources} ${_header})
-      
+
    endforeach (_current_FILE)
 endmacro (KDE4_ADD_PLASMA_UI_FILES)
 
