1f7271f156bfc75c55d9711ed0ac82ec0640c625
Ivan Čukić <ivan.cukic@kde.org>
2008-05-16 19:14:08 +0000

    UI file strings that need to be translated
    svn path=/trunk/extragear/plasma/; revision=808504

---
+++ applets/lancelot/app/src/i18n_strings.cpp
@@ -0,0 +1,14 @@
+// This file only contains strings that need to be localized.
+// The strings are extracted from Plasma UI file
+
+I18N_NOOP("Documents");
+I18N_NOOP("Contacts");
+I18N_NOOP("Computer");
+I18N_NOOP("Applications");
+I18N_NOOP("Computer");
+I18N_NOOP("Contacts");
+I18N_NOOP("Documents");
+I18N_NOOP("Search");
+I18N_NOOP("Lock Session");
+I18N_NOOP("Log Out...");
+I18N_NOOP("Switch User");
