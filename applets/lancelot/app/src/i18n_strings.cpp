723ded49d93a25087c1d434249c0c2c6a377257f
Ivan Čukić <ivan.cukic@kde.org>
2008-05-17 17:22:24 +0000

    Added a couple of strings that will need to be translated Removed ... from Log Out button title
    svn path=/trunk/extragear/plasma/; revision=808850

--- applets/lancelot/app/src/i18n_strings.cpp
+++ applets/lancelot/app/src/i18n_strings.cpp
@@ -1,4 +1,5 @@
 // This file only contains strings that need to be localized.
+
 // The strings are extracted from Plasma UI file
 
 I18N_NOOP("Documents");
@@ -10,5 +11,11 @@ I18N_NOOP("Contacts");
 I18N_NOOP("Documents");
 I18N_NOOP("Search");
 I18N_NOOP("Lock Session");
-I18N_NOOP("Log Out...");
+I18N_NOOP("Log Out");
 I18N_NOOP("Switch User");
+
+// The strings that will be needed in the future
+I18N_NOOP("needed program is not running", "%1 is not running");
+I18N_NOOP("start a program", "start %1");
+I18N_NOOP("Online contacts");
+I18N_NOOP("e-Mail contacts");
