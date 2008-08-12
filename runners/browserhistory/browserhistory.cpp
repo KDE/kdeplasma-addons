f1e9e32fc69a318c8ce86d4434f257dead6922cc
Sebastian Kügler <sebas@kde.org>
2008-08-12 07:29:03 +0000

    browserhistory is a new runner for KRunner that searches through the webbrowser history.
    This is roughly what's been reviewed on plasma-devel, I'll be
    fixing some things in the next commits.
    
    svn path=/trunk/KDE/kdeplasma-addons/; revision=845629

---
+++ runners/browserhistory/browserhistory.cpp
@@ -0,0 +1,97 @@
+/*
+ *   Copyright 2008 Sebastian Kügler
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "browserhistory.h"
+
+#include <QDBusInterface>
+#include <QDBusReply>
+
+#include <KDebug>
+#include <KStandardDirs>
+#include <KToolInvocation>
+#include <KIcon>
+#include <KConfig>
+#include <KConfigGroup>
+
+// Update history at most every 30 seconds
+#define UPDATE_INTERVAL 30000
+
+BrowserHistoryRunner::BrowserHistoryRunner(QObject *parent, const QVariantList& args)
+    : Plasma::AbstractRunner(parent, args)
+{
+    Q_UNUSED(args);
+    KGlobal::locale()->insertCatalog("krunner_browserhistoryrunner");
+    setObjectName(i18n("Browser History"));
+    m_icon = KIcon("view-history");
+    // What is this about? The API doc
+    setIgnoredTypes(Plasma::RunnerContext::FileSystem);
+    m_history = loadHistory();
+}
+
+BrowserHistoryRunner::~BrowserHistoryRunner()
+{
+}
+
+QStringList BrowserHistoryRunner::loadHistory()
+{
+    m_time.restart();
+    KConfig *konq_config = new KConfig("konq_history", KConfig::NoGlobals);
+    KConfigGroup locationBarGroup( konq_config, "Location Bar" );
+    return locationBarGroup.readPathEntry( "ComboContents", QStringList() );
+}
+
+void BrowserHistoryRunner::match(Plasma::RunnerContext &context)
+{
+    const QString term = context.query();
+    if (term.length() < 3) {
+        return;
+    }
+    if (m_time.elapsed() > UPDATE_INTERVAL) {
+        //kDebug() << "===========TIME:" << m_time.elapsed() << "[" << UPDATE_INTERVAL << "]";
+        loadHistory();
+    }
+    foreach (const QString &historyitem, m_history) {
+        // Filter out error pages, and match ...
+        if (!historyitem.startsWith("error:/") && historyitem.contains(term, Qt::CaseInsensitive)) {
+            kDebug() << "MATCH:" << term << historyitem;
+            Plasma::QueryMatch match(this);
+            match.setType(Plasma::QueryMatch::CompletionMatch);
+            match.setRelevance(0.5);
+            match.setIcon(m_icon);
+            match.setData(historyitem);
+            match.setText(historyitem);
+            context.addMatch(term, match);
+            kDebug() << "addMatch" << match.data().toString();
+        }
+    }
+}
+
+void BrowserHistoryRunner::run(const Plasma::RunnerContext &context, const Plasma::QueryMatch &match)
+{
+    kDebug() << "run run run run run run run run run run";
+    Q_UNUSED(context)
+    QString location = match.data().toString();
+    kDebug() << location;
+    if (!location.isEmpty()) {
+        KToolInvocation::invokeBrowser(location);
+        kDebug() << "BLA" << location;
+    }
+}
+
+#include "browserhistory.moc"
---
+++ runners/browserhistory/browserhistory.h
@@ -0,0 +1,46 @@
+/*
+ *   Copyright 2008 Sebastian Kügler
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef BROWSERHISTORY_H
+#define BROWSERHISTORY_H
+
+#include <plasma/abstractrunner.h>
+
+#include <KIcon>
+
+class BrowserHistoryRunner : public Plasma::AbstractRunner {
+    Q_OBJECT
+
+    public:
+        BrowserHistoryRunner( QObject *parent, const QVariantList& args );
+        ~BrowserHistoryRunner();
+
+        void match(Plasma::RunnerContext &context);
+        void run(const Plasma::RunnerContext &context, const Plasma::QueryMatch &match);
+
+    private:
+        QStringList loadHistory();
+        KIcon m_icon;
+        QStringList m_history;
+        QTime m_time;
+};
+
+K_EXPORT_PLASMA_RUNNER(krunner_browserhistory, BrowserHistoryRunner)
+
+#endif
