ab6bdc2e3790dbbaf014094ca74a920d6be985b5
Laurent Montel <montel@kde.org>
2008-08-13 09:41:04 +0000

    Restore default picture (QSvgRenderer doesn't support zipped file (will support in qt4.5 I think, but not qt4.4.1))
    svn path=/trunk/KDE/kdeplasma-addons/; revision=846220

--- applets/frame/picture.cpp
+++ applets/frame/picture.cpp
@@ -21,7 +21,7 @@
 #include "picture.h"
 
 #include <QPainter>
-#include <QSvgRenderer>
+#include <KSvgRenderer>
 #include <QTextOption>
 
 #include <KUrl>
@@ -41,13 +41,13 @@ QImage Picture::defaultPicture(const QString &message)
 {
     // Create a QImage with same axpect ratio of default svg and current pixelSize
     QString svgFile = Plasma::Theme::defaultTheme()->imagePath("widgets/picture-frame-default");
-    QSvgRenderer sr(svgFile);
+    KSvgRenderer sr(svgFile);
     QImage imload(sr.defaultSize(),QImage::Format_RGB32);//TODO optimize, too slow
-   
+
     QPainter p(&imload);
     sr.render(&p, QRect(QPoint(0, 0), imload.size()));
 
-    // Set the font and draw text 
+    // Set the font and draw text
     p.setRenderHint(QPainter::Antialiasing);
     QFont textFont;
     textFont.setPixelSize(imload.height() / 12);
@@ -72,7 +72,7 @@ QImage Picture::setPicture(const KUrl &currentUrl)
         if (tempImage.isNull()){
             m_picture = defaultPicture(i18nc("Error", "Error loading image"));
             return m_picture;
-        } else { // Load success! Scale the image if it is too big	    
+        } else { // Load success! Scale the image if it is too big
             if (tempImage.width() > m_maxDimension || tempImage.height() > m_maxDimension) {
                 m_picture = tempImage.scaled(m_maxDimension,m_maxDimension,
                                              Qt::KeepAspectRatio,Qt::SmoothTransformation);
