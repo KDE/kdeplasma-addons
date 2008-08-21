b49da056c08ccd6b0c008384dbc94907cef1176e
Sebastian Kügler <sebas@kde.org>
2008-08-21 14:21:14 +0000

    License header should bear the email address.
    svn path=/trunk/KDE/kdeplasma-addons/; revision=850439

--- runners/browserhistory/browserhistory.h
+++ runners/browserhistory/browserhistory.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2008 Sebastian Kügler
+ *   Copyright 2008 Sebastian Kügler <sebas@kde.org>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Library General Public License as
