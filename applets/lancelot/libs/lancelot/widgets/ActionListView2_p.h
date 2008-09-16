62eed0bccc7a1a35e8de8a8333430d639afb4e19
Ivan Čukić <ivan.cukic@kde.org>
2008-09-16 07:39:12 +0000

    File forgotten
    svn path=/trunk/KDE/kdeplasma-addons/; revision=861451

---
+++ applets/lancelot/libs/lancelot/widgets/ActionListView2_p.h
@@ -0,0 +1,101 @@
+/*
+ *   Copyright (C) 2007 Ivan Cukic <ivan.cukic+kde@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser/Library General Public License version 2,
+ *   or (at your option) any later version, as published by the Free
+ *   Software Foundation
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser/Library General Public License for more details
+ *
+ *   You should have received a copy of the GNU Lesser/Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef LANCELOT_ACTION_LIST_VIEW2_PH_
+#define LANCELOT_ACTION_LIST_VIEW2_PH_
+
+#include <lancelot/lancelot.h>
+#include <lancelot/lancelot_export.h>
+
+#include <QtGui>
+#include <QtCore>
+#include <KDebug>
+#include <QIcon>
+
+#include <lancelot/widgets/CustomListView.h>
+#include <lancelot/models/ActionListViewModels.h>
+
+namespace Lancelot
+{
+
+//> ActionListView2Item
+class ActionListView2Item: public ExtenderButton, public CustomListItem {
+public:
+    ActionListView2Item();
+    ~ActionListView2Item();
+
+    L_Override virtual void setSelected(bool selected = true);
+    L_Override virtual bool isSelected() const;
+
+private:
+    bool m_selected;
+};
+//<
+
+//> ActionListView2ItemFactory
+class ActionListView2ItemFactory: public CustomListItemFactory {
+    Q_OBJECT;
+public:
+    ActionListView2ItemFactory(ActionListViewModel * model);
+    ~ActionListView2ItemFactory();
+
+    L_Override virtual CustomListItem * itemForIndex(int index);
+    L_Override virtual CustomListItem * itemForIndex(int index,
+            bool reload);
+    L_Override int itemCount() const;
+    L_Override virtual int itemHeight(int index, Qt::SizeHint which) const;
+
+    bool categoriesActivable() const;
+
+    void setCategoriesActivable(bool value);
+
+    void setModel(ActionListViewModel * model);
+
+    ActionListViewModel * model();
+
+private:
+    void reload();
+
+private Q_SLOTS:
+    void modelUpdated();
+    void modelItemInserted(int index);
+    void modelItemDeleted(int index);
+    void modelItemAltered(int index);
+
+private:
+    ActionListViewModel * m_model;
+    QList < ActionListView2Item * > m_items;
+    bool m_categoriesActivable : 1;
+};
+//<
+
+//> ActionListView2
+class ActionListView2::Private {
+public:
+    Private();
+    ~Private();
+
+    ActionListView2ItemFactory * itemFactory;
+};
+//<
+
+} // namespace Lancelot
+
+#endif /* LANCELOT_ACTION_LIST_VIEW2_PH_ */
+
