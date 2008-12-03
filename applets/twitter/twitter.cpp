04e09a46e4ed9333913d00cf85afbf6095525dae
Marco Martin <notmart@gmail.com>
2008-12-03 19:24:41 +0000

    still some layout tweak..
    svn path=/trunk/KDE/kdeplasma-addons/; revision=892191

--- applets/twitter/twitter.cpp
+++ applets/twitter/twitter.cpp
@@ -523,7 +523,7 @@ void Twitter::showTweets()
     qreal left, top, right, bottom;
     getContentsMargins(&left, &top, &right, &bottom);
     //this line break things, strange
-    //m_graphicsWidget->setMinimumSize(m_layout->sizeHint(Qt::MinimumSize));
+    m_graphicsWidget->setMinimumSize(m_layout->sizeHint(Qt::MinimumSize));
     m_graphicsWidget->setPreferredSize(m_layout->sizeHint(Qt::PreferredSize));
     //are we complete?
     if (layout()->itemAt(0) == m_graphicsWidget) {
@@ -532,7 +532,6 @@ void Twitter::showTweets()
         setPreferredSize(m_layout->sizeHint(Qt::PreferredSize) + QSizeF(left+right, top+bottom));
         resize(preferredSize());
     }
-    adjustSize();
 }
 
 void Twitter::createConfigurationInterface(KConfigDialog *parent)
