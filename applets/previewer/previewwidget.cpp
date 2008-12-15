e0fb40c6ec21c8f2561691fbb6f02239b634843e
Aaron J. Seigo <aseigo@kde.org>
2008-12-15 20:34:31 +0000

    adjust to api
    svn path=/trunk/KDE/kdeplasma-addons/; revision=897342

--- applets/previewer/previewwidget.cpp
+++ applets/previewer/previewwidget.cpp
@@ -50,8 +50,8 @@ PreviewWidget::PreviewWidget(QGraphicsItem *parent)
       m_closeStatus(true),
       m_animId(-1)
 {
-    m_scrollBar = new Plasma::ScrollBar(Qt::Vertical, this);
-    connect(m_scrollBar->nativeWidget(), SIGNAL(valueChanged(int)), this, SLOT(scrolled( int )));
+    m_scrollBar = new Plasma::ScrollBar(this);
+    connect(m_scrollBar, SIGNAL(valueChanged(int)), this, SLOT(scrolled( int )));
     m_scrollBar->setSingleStep(10);
     m_scrollBar->hide();
 
