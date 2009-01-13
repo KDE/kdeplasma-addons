1110eaee28a438668af5e06b40b461be333f8e83
Anne-Marie Mahfouf <annma@kde.org>
2009-01-13 12:11:41 +0000

    add context info to help the translators I hope I did not mess the indentation too much, and also the encoding, I have trouble with Kate set as utf-8 to Open/save but after opening a utf-8 file it sets then encoding to some ISO...
    svn path=/trunk/KDE/kdeplasma-addons/; revision=910444

--- libs/conversion/length.cpp
+++ libs/conversion/length.cpp
@@ -28,34 +28,34 @@ Length::Length(QObject* parent)
 
     addSIUnit(m_default, i18n("meter"), i18n("meters"));
 
-    m_units[i18n("inch")]          = i18n("inches");
-    m_units[i18n("in")]            = i18n("inches");
-    m_units[i18n("\"")]            = i18n("inches");
-    m_units[i18n("inches")]        = 0.0254;
-    m_units[i18n("foot")]          = i18n("feet");
-    m_units[i18n("ft")]            = i18n("feet");
-    m_units[i18n("feet")]          = 0.3048;
-    m_units[i18n("yard")]          = i18n("yards");
-    m_units[i18n("yd")]            = i18n("yards");
-    m_units[i18n("yards")]         = 0.9144;
-    m_units[i18n("mile")]          = i18n("miles");
-    m_units[i18n("ml")]            = i18n("miles");
-    m_units[i18n("miles")]         = 1609.344;
-    m_units[i18n("nautical mile")] = i18n("nautical miles");
-    m_units[i18n("nmi")]           = i18n("nautical miles");
+    m_units[i18n("inch")]          			= i18n("inches");
+    m_units[i18nc("length unit: inch","in")]            = i18n("inches");
+    m_units[i18nc("length unit: inch","\"")]            = i18n("inches");
+    m_units[i18n("inches")]        			= 0.0254;
+    m_units[i18nc("length unit: foot","foot")]          = i18n("feet");
+    m_units[i18nc("length unit: foot","ft")]            = i18n("feet");
+    m_units[i18n("feet")]          			= 0.3048;
+    m_units[i18n("yard")]          			= i18n("yards");
+    m_units[i18nc("length unit: yard","yd")]            = i18n("yards");
+    m_units[i18n("yards")]         			= 0.9144;
+    m_units[i18nc("length unit","mile")]          	= i18n("miles");
+    m_units[i18nc("length unit: mile","ml")]            = i18n("miles");
+    m_units[i18n("miles")]         			= 1609.344;
+    m_units[i18n("nautical mile")] 			= i18n("nautical miles");
+    m_units[i18nc("length unit: nautical mile","nmi")]  = i18n("nautical miles");
     m_units[i18n("nautical miles")] = 1852.0;
 
-    m_units[i18n("ly")]            = "lightyears";
-    m_units[i18n("light-year")]    = "lightyears";
-    m_units[i18n("light-years")]   = "lightyears";
-    m_units[i18n("lightyear")]     = "lightyears";
-    m_units[i18n("lightyears")]    = 9460730472580800.0;
-    m_units[i18n("pc")]            = "parsecs";
-    m_units[i18n("parsec")]        = "parsecs";
-    m_units[i18n("parsecs")]       = 3.085678E+16;
-    m_units[i18n("au")]            = "astronomical units";
-    m_units[i18n("astronomical unit")]  = "astronomical units";
-    m_units[i18n("astronomical units")] = 149597870691.0;
+    m_units[i18nc("length unit: light-year","ly")]      = "lightyears";
+    m_units[i18n("light-year")]    			= "lightyears";
+    m_units[i18n("light-years")]   			= "lightyears";
+    m_units[i18n("lightyear")]     			= "lightyears";
+    m_units[i18n("lightyears")]    			= 9460730472580800.0;
+    m_units[i18nc("length unit: parsec","pc")]          = "parsecs";
+    m_units[i18n("parsec")]        			= "parsecs";
+    m_units[i18n("parsecs")]       			= 3.085678E+16;
+    m_units[i18nc("length unit: astronomical unit","au")] = "astronomical units";
+    m_units[i18n("astronomical unit")]  		= "astronomical units";
+    m_units[i18n("astronomical units")] 		= 149597870691.0;
 }
 
 QString Length::name() const
