e8a2a019985100ed0b4d4b96058db997627e313e
Anne-Marie Mahfouf <annma@kde.org>
2009-02-20 14:27:53 +0000

    enable date posted to be shown, the QLocale is for it to work for non English users
    svn path=/trunk/KDE/kdeplasma-addons/; revision=929001

--- applets/microblog/microblog.cpp
+++ applets/microblog/microblog.cpp
@@ -511,9 +511,10 @@ void MicroBlog::showTweets()
             sourceString = i18n(" from %1", tweetData.value( "Source" ).toString());
         }
 
+	QLocale english(QLocale::English, QLocale::UnitedStates);
         QString html = "<table cellspacing='0' spacing='5' width='100%'>";
         html += QString("<tr height='1em'><td align='left' width='1%'><font color='%2'>%1</font></td><td align='right' width='auto'><p align='right'><font color='%2'>%3%4</font></p></td></tr>").arg( user).arg(m_colorScheme->foreground(KColorScheme::InactiveText).color().name())
-                .arg(timeDescription( tweetData.value( "Date" ).toDateTime() )).arg( sourceString);
+                .arg(timeDescription( english.toDateTime(tweetData.value( "Date" ).toString(), "ddd MMM dd HH:mm:ss +0000 yyyy") )).arg( sourceString);
         QString status = tweetData.value( "Status" ).toString();
 
         status.replace(QRegExp("((http|https)://[^\\s<>'\"]+[^!,\\.\\s<>'\"\\]])"), "<a href='\\1'>\\1</a>");
