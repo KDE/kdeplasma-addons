b53e8dc77359c9c57cb707a50372af48b66c69bc
Burkhard Lück <lueck@hube-lueck.de>
2009-07-08 20:58:29 +0000

    rename catalogs to make plasma translatable
    svn path=/branches/KDE/4.3/kdeplasma-addons/; revision=993537

--- applets/charselect/Messages.sh
+++ applets/charselect/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT *.cpp -o $podir/plasma_applet_charselect.pot
+$XGETTEXT *.cpp -o $podir/plasma_applet_CharSelectApplet.pot
--- applets/microblog/Messages.sh
+++ applets/microblog/Messages.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/env bash
-$XGETTEXT *.cpp -o $podir/plasma_applet_twitter.pot
+$XGETTEXT *.cpp -o $podir/plasma_applet_microblog.pot
