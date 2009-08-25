818988537e5492044e0431e911ebed967c2667fd
Eckhart Wörner <ewoerner@kde.org>
2009-08-25 11:42:51 +0000

    Add an activity list widget, this will replace the old activity management in the applet class
    svn path=/trunk/KDE/kdeplasma-addons/; revision=1015371

---
+++ applets/opendesktop/activitylist.cpp
@@ -0,0 +1,108 @@
+/*
+    This file is part of KDE.
+
+    Copyright (c) 2009 Eckhart Wörner <ewoerner@kde.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+    USA.
+*/
+
+#include "activitylist.h"
+
+#include "activitywidget.h"
+
+
+ActivityList::ActivityList(Plasma::DataEngine* engine, QGraphicsWidget* parent)
+    : ScrollWidget(parent),
+      m_engine(engine),
+      m_limit(20),
+      m_updateInterval(5 * 60)
+{
+    m_container = new QGraphicsWidget(this);
+    m_layout = new QGraphicsLinearLayout(Qt::Vertical, m_container);
+    setWidget(m_container);
+
+    m_engine->connectSource("activity", this, m_updateInterval * 1000);
+    dataUpdated("activity", m_engine->query("activity"));
+}
+
+
+int ActivityList::limit() const
+{
+    return m_limit;
+}
+
+
+int ActivityList::updateInterval() const
+{
+    return m_updateInterval;
+}
+
+
+void ActivityList::setLimit(int limit)
+{
+    if (limit != m_limit) {
+        m_limit = limit;
+        dataUpdated("activity", m_engine->query("activity"));
+    }
+}
+
+
+void ActivityList::setUpdateInterval(int interval)
+{
+    m_updateInterval = interval;
+    m_engine->connectSource("activity", this, m_updateInterval * 1000);
+}
+
+
+QStringList ActivityList::getDisplayedActivities(const Plasma::DataEngine::Data& data) {
+    // FIXME: This should take into order filtering, sorting etc.
+    QStringList result;
+    foreach (const QString& key, data.keys()) {
+        if (result.size() >= m_limit) {
+            break;
+        }
+        result.append(key);
+    }
+    return result;
+}
+
+
+void ActivityList::dataUpdated(const QString& source, const Plasma::DataEngine::Data& data)
+{
+    Q_UNUSED(source)
+    
+    QStringList displayedActivities = getDisplayedActivities(data);
+    
+    // FIXME: This is still highly inefficient
+    while (m_layout->count()) {
+        ActivityWidget* widget = static_cast<ActivityWidget*>(m_layout->itemAt(0));
+        m_layout->removeAt(0);
+        widget->deleteLater();
+    }
+
+    QStringList::iterator j = displayedActivities.begin();
+    for (int i = 0; i < displayedActivities.size(); ++i, ++j) {
+        ActivityWidget* widget = new ActivityWidget(m_container);
+        widget->setActivityData(data[*j].value<Plasma::DataEngine::Data>());
+        m_layout->addItem(widget);
+    }
+    
+    // Go to the top of the list
+    this->setPos(0, 0);
+}
+
+
+#include "activitylist.moc"
---
+++ applets/opendesktop/activitylist.h
@@ -0,0 +1,84 @@
+/*
+    This file is part of KDE.
+
+    Copyright (c) 2009 Eckhart Wörner <ewoerner@kde.org>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+    USA.
+*/
+
+#ifndef ACTIVITYLIST_H
+#define ACTIVITYLIST_H
+
+#include <QtCore/QString>
+#include <QtGui/QGraphicsLinearLayout>
+
+#include <Plasma/DataEngine>
+#include <Plasma/ScrollWidget>
+
+
+class ActivityWidget;
+
+class ActivityList : public Plasma::ScrollWidget
+{
+    Q_OBJECT
+    
+    public:
+        /**
+         * Creates a new ActivityList widget
+         * @param engine the Plasma data engine to use
+         * @param parent the parent of this widget
+         */
+        ActivityList(Plasma::DataEngine* engine, QGraphicsWidget* parent = 0);
+
+        /**
+         * Returns the maximum number of items shown
+         * @return the current limit
+         */
+        int limit() const;
+
+        /**
+         * Returns the interval for refreshing the activities list
+         * @return the current interval in seconds
+         */
+        int updateInterval() const;
+
+        /**
+         * Adjusts the maximum number of items shown
+         * @param limit the new limit
+         */
+        void setLimit(int limit);
+
+        /**
+         * Adjusts the interval for refreshing the activities list
+         * @param interval the new interval in seconds
+         */
+        void setUpdateInterval(int interval);
+
+    private Q_SLOTS:
+        void dataUpdated(const QString& source, const Plasma::DataEngine::Data& data);
+
+    private:
+        QStringList getDisplayedActivities(const Plasma::DataEngine::Data& data);
+        
+        QGraphicsWidget* m_container;
+        Plasma::DataEngine* m_engine;
+        QGraphicsLinearLayout* m_layout;
+        int m_limit;
+        int m_updateInterval;
+};
+
+
+#endif
