1f254cbc1e8b5aa75ed7272f9921057df23b4fdb
Eckhart Wörner <ewoerner@kde.org>
2009-09-10 11:52:40 +0000

    SVN_SILENT Fix typo in copyright headers, stupid c&p
    svn path=/trunk/KDE/kdeplasma-addons/; revision=1021967

--- dataengines/ocs/lib/eventlistjob.h
+++ dataengines/ocs/lib/eventlistjob.h
@@ -1,7 +1,7 @@
 /*
     This file is part of KDE.
 
-    Copyright (c) 2009 Eckhart Wörner <ewoerner@kde.org
+    Copyright (c) 2009 Eckhart Wörner <ewoerner@kde.org>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
