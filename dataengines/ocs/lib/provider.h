db28c3839655279da0dbc542e9e650eb2615f62c
Eckhart Wörner <ewoerner@kde.org>
2009-09-10 11:55:35 +0000

    Add missing copyright notice
    svn path=/trunk/KDE/kdeplasma-addons/; revision=1021968

--- dataengines/ocs/lib/provider.h
+++ dataengines/ocs/lib/provider.h
@@ -1,6 +1,8 @@
 /*
     This file is part of KDE.
 
+    Copyright (c) 2008 Cornelius Schumacher <schumacher@kde.org>
+
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
