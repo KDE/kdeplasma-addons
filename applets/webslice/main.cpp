4289826a1ef9edb59b4803eaea1784498b890304
Sebastian Kügler <sebas@kde.org>
2009-11-25 01:19:13 +0000

    minor cleanup
    svn path=/trunk/KDE/kdeplasma-addons/; revision=1053924

--- applets/webslice/main.cpp
+++ applets/webslice/main.cpp
@@ -8,10 +8,8 @@ int main( int argc, char **argv )
 
     KWebSlice slice;
     slice.setElement( QString("#hotspot") );
-    //slice.setElement( QString("#box") );
     slice.setLoadingText("<h1>Loading ...<h1>");
     slice.setUrl( QUrl(QString("http://www.kde.org/")) );
-    //slice.setUrl( QUrl("file:///home/sebas/kdesvn/install/share/apps/silk-webapp/test/testsite/beach.html") );
     slice.show();
 
     return app.exec();
