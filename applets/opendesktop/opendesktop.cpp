3a2a426c618ba5195fe048fb0721fc7b45c586ac
Anne-Marie Mahfouf <annma@kde.org>
2009-12-05 17:18:25 +0000

    save as proper UTF-8
    svn path=/trunk/KDE/kdeplasma-addons/; revision=1059054

--- applets/opendesktop/opendesktop.cpp
+++ applets/opendesktop/opendesktop.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright 2009 by Sebastian K?gler <sebas@kde.org>                    *
+ *   Copyright 2009 by Sebastian Kügler <sebas@kde.org>                    *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
