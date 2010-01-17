eb94b06afaa3958c17e60877308308d6a9a60e82
Ivan Čukić <ivan.cukic@kde.org>
2010-01-17 14:06:01 +0000

    Switched license to LGPL
    svn path=/trunk/KDE/kdeplasma-addons/; revision=1076098

--- applets/lancelot/libs/lancelot-datamodels/SystemActions.cpp
+++ applets/lancelot/libs/lancelot-datamodels/SystemActions.cpp
@@ -1,18 +1,18 @@
 /*
- *   Copyright (C) 2007 Ivan Cukic <ivan.cukic(at)kde.org>
+ *   Copyright (C) 2007, 2008, 2009, 2010 Ivan Cukic <ivan.cukic(at)kde.org>
  *   Copyright (C) 2006 Aaron Seigo <aseigo@kde.org>
  *
  *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License version 2,
+ *   it under the terms of the GNU Lesser/Library General Public License version 2,
  *   or (at your option) any later version, as published by the Free
  *   Software Foundation
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details
+ *   GNU Lesser/Library General Public License for more details
  *
- *   You should have received a copy of the GNU General Public
+ *   You should have received a copy of the GNU Lesser/Library General Public
  *   License along with this program; if not, write to the
  *   Free Software Foundation, Inc.,
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
