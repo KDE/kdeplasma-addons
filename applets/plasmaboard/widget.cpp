1d5e93df61b6de9e5d11ab6dfb1347a683e6ba4b
Björn Ruberg <bjoern@ruberg-wegener.de>
2010-04-02 22:35:44 +0000

    Keyboard is not clipped at the bottom anymore
    svn path=/trunk/KDE/kdeplasma-addons/; revision=1110390

--- applets/plasmaboard/widget.cpp
+++ applets/plasmaboard/widget.cpp
@@ -446,7 +446,7 @@ void PlasmaboardWidget::press(FuncKey *key)
 void PlasmaboardWidget::refreshKeys()
 {
     double factor_x = size().width() / 10000;
-    double factor_y = size().height() / 10000;
+    double factor_y = size().height() / 10001;
 
     Q_FOREACH(BoardKey* key, m_keys){
         key->updateDimensions(factor_x, factor_y);
