8eae49951bbf480536642bf684ac4998c1ed4638
Sandro Andrade <sandroandrade@kde.org>
2010-04-10 22:07:01 +0000

    - Added scripts for data consolidation. - Initial data acquisition servlet.
    svn path=/trunk/playground/base/plasma/dataengines/kdecommits/; revision=1113442

---
+++ dataengines/kdecommits/servlet/KdeCommitsServlet.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class KdeCommitsServlet extends HttpServlet
+{
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        String dbHost = "localhost";
+        String dbName = "sandros_kde";
+        String username = "sandros_scripty";
+        String password = "oBse_4s";
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World!</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World!</h1>");
+
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch(ClassNotFoundException msg)
+        {
+            out.println("Error loading driver:" + msg.getMessage());
+        }
+
+        try
+        {
+            String url ="jdbc:mysql://" + dbHost + ":3306/" + dbName;
+            Connection Conn = DriverManager.getConnection(url, username, password);
+            Statement Stmt = Conn.createStatement();
+            String query = "select * from developers";
+            ResultSet res = Stmt.executeQuery(query);
+            out.println("Query result :</br>");
+            while(res.next())
+            {
+                out.println(res.getObject(1) + " - " + res.getObject(2) + " - " + res.getObject(3) + " - " + res.getObject(4));
+            }
+            out.println("</br>");
+        }
+        catch(SQLException e)
+        {
+            String err1Msg = e.getMessage();
+        }
+
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
