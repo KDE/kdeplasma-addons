40f504628037c4fd683be0ef466918f5d2d93f20
Ivan Čukić <ivan.cukic@kde.org>
2010-04-16 10:44:34 +0000

    Updated versions
    svn path=/trunk/KDE/kdeplasma-addons/; revision=1115398

--- applets/lancelot/application/AboutData.cpp
+++ applets/lancelot/application/AboutData.cpp
@@ -24,7 +24,7 @@ AboutData::AboutData()
             QByteArray("lancelot"),
             QByteArray(""),
             ki18n("Lancelot"),
-            QByteArray("1.8"),
+            QByteArray("1.9.5"),
             ki18n("Next-generation application launcher."),
             KAboutData::License_GPL,
             ki18n("(C) 2007, 2008, 2009, 2010 Ivan Čukić"),
