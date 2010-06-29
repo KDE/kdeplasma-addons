33def06e9414557526c2d098494edbeeeceb0165
Giulio Camuffo <giuliocamuffo@gmail.com>
2010-06-29 14:07:13 +0000

    caused more troubles than what it fixed
    svn path=/trunk/playground/base/plasma/containments/groupingdesktop/; revision=1144220

--- containments/groupingdesktop/lib/gridgroup.cpp
+++ containments/groupingdesktop/lib/gridgroup.cpp
@@ -567,9 +567,8 @@ bool GridGroup::eventFilter(QObject *, QEvent *event)
 int GridGroup::isOnAColumnBorder(qreal x) const
 {
     qreal pos = 0;
-    int gap = 0;
+    int gap = 20;
     for (int i = 0; i < m_columnWidths.size(); ++i) {
-        gap = m_columnWidths.at(i) * contentsRect().width() / 2;
         if (pos > x - gap && pos < x + gap) {
             return i;
         }
@@ -585,9 +584,8 @@ int GridGroup::isOnAColumnBorder(qreal x) const
 int GridGroup::isOnARowBorder(qreal y) const
 {
     qreal pos = 0;
-    int gap = 0;
+    int gap = 20;
     for (int i = 0; i < m_rowHeights.size(); ++i) {
-        gap = m_rowHeights.at(i) * contentsRect().height() / 2;
         if (pos > y - gap && pos < y + gap) {
             return i;
         }
