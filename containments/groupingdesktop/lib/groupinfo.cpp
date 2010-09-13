bf1e5e39fc962d792f92987c17feb8c510623fd5
Giulio Camuffo <giuliocamuffo@gmail.com>
2010-09-13 13:47:13 +0000

    whoops
    svn path=/trunk/KDE/kdeplasma-addons/; revision=1174887

---
+++ containments/groupingdesktop/lib/groupinfo.cpp
@@ -0,0 +1,98 @@
+/*
+ *   Copyright 2010 by Giulio Camuffo <giuliocamuffo@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "groupinfo.h"
+
+class GroupInfoPrivate
+{
+    public:
+        GroupInfoPrivate()
+        {
+            formFactors << Plasma::Planar << Plasma::MediaCenter;
+        }
+
+        ~GroupInfoPrivate()
+        {}
+
+        QString name;
+        QString prettyName;
+        QSet<Plasma::FormFactor> formFactors;
+        QString icon;
+};
+
+GroupInfo::GroupInfo(const QString &name, const QString &prettyName)
+          : d(new GroupInfoPrivate())
+{
+    d->name = name;
+    d->prettyName = prettyName;
+}
+
+GroupInfo::~GroupInfo()
+{
+
+}
+
+void GroupInfo::setFormFactors(QSet<Plasma::FormFactor> formFactors)
+{
+    d->formFactors = formFactors;
+}
+
+void GroupInfo::setIcon(const QString &icon)
+{
+    d->icon = icon;
+}
+
+QString GroupInfo::name() const
+{
+    return d->name;
+}
+
+QString GroupInfo::prettyName() const
+{
+    return d->prettyName;
+}
+
+QSet<Plasma::FormFactor> GroupInfo::formFactors() const
+{
+    return d->formFactors;
+}
+
+QString GroupInfo::icon() const
+{
+    return d->icon;
+}
+
+GroupInfo GroupInfo::operator=(const GroupInfo &gi)
+{
+    GroupInfo g(gi.name(), gi.prettyName());
+    g.setFormFactors(gi.formFactors());
+    g.setIcon(gi.icon());
+
+    return g;
+}
+
+bool GroupInfo::operator==(const GroupInfo &gi) const
+{
+    return (gi.name() == d->name);
+}
+
+bool GroupInfo::operator<(const GroupInfo &gi) const
+{
+    return (gi.name() < d->name);
+}
---
+++ containments/groupingdesktop/lib/groupinfo.h
@@ -0,0 +1,51 @@
+/*
+ *   Copyright 2010 by Giulio Camuffo <giuliocamuffo@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef GROUPINFO_H
+#define GROUPINFO_H
+
+#include <QtCore/QString>
+#include <QtCore/QSet>
+
+#include <Plasma/Plasma>
+
+class GroupInfoPrivate;
+
+class GroupInfo
+{
+    public:
+        GroupInfo(const QString &name, const QString &prettyName = QString());
+        ~GroupInfo();
+        void setFormFactors(QSet<Plasma::FormFactor> formFactors);
+        void setIcon(const QString &icon);
+
+        QString name() const;
+        QString prettyName() const;
+        QSet<Plasma::FormFactor> formFactors() const;
+        QString icon() const;
+
+        GroupInfo operator=(const GroupInfo &gi);
+        bool operator==(const GroupInfo &gi) const;
+        bool operator<(const GroupInfo &gi) const;
+
+    private:
+        GroupInfoPrivate *const d;
+};
+
+#endif //GROUPINFO_H
