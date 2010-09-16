3a0509725858acc2b96b63d0acfebdf9280662f4
Giulio Camuffo <giuliocamuffo@gmail.com>
2010-09-16 13:20:10 +0000

    don't show the panel if the immutability is not Mutable
    svn path=/trunk/KDE/kdeplasma-addons/; revision=1176036

--- containments/groupingdesktop/lib/groups/gridgroup.cpp
+++ containments/groupingdesktop/lib/groups/gridgroup.cpp
@@ -248,7 +248,7 @@ void GridGroup::resizeEvent(QGraphicsSceneResizeEvent *event)
 void GridGroup::hoverMoveEvent(QGraphicsSceneHoverEvent *event)
 {
     QPointF pos(event->pos());
-    if (m_gridManager->geometry().contains(pos)) {
+    if (m_gridManager->geometry().contains(pos) || immutability() != Plasma::Mutable) {
         return;
     }
     QRectF bRect(boundingRect());
