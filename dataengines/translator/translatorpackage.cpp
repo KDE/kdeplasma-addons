3fca1350f2297cb819a369542955d535b2735bb1
Farhad Hedayati Fard <hf.farhad@gmail.com>
2011-03-17 15:33:20 +0330

    fixed coding style

--- dataengines/translator/translatorpackage.cpp
+++ dataengines/translator/translatorpackage.cpp
@@ -1,5 +1,4 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
     Copyright (C) 2011  Farhad Hedayati-Fard <hf.farhad@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
@@ -23,21 +22,21 @@
 #include "plasma/package.h"
 
 TranslatorPackage::TranslatorPackage( QObject *parent, const QVariantList& args )
-    : Plasma::PackageStructure( parent, QLatin1String( "Plasma/Translator" ) )
+: Plasma::PackageStructure( parent, QLatin1String( "Plasma/Translator" ) )
 {
     Q_UNUSED( args )
     addDirectoryDefinition( "images", QLatin1String( "images" ), i18n( "Images" ) );
     QStringList mimetypes;
     mimetypes << QLatin1String( "image/svg+xml" ) << QLatin1String( "image/png" ) << QLatin1String( "image/jpeg" );
     setMimetypes( "images", mimetypes );
-
+    
     addDirectoryDefinition( "scripts", QLatin1String( "code" ), i18n( "Executable Scripts" ) );
     mimetypes.clear();
     mimetypes << QLatin1String( "text/*" );
     setMimetypes( "scripts", mimetypes );
-
+    
     addFileDefinition( "mainscript", QLatin1String( "code/main" ), i18n( "Main Script File" ) );
-
+    
     setDefaultPackageRoot( QLatin1String( "plasma/translator/" ) );
     setServicePrefix( QLatin1String( "plasma-translator-" ) );
 }
