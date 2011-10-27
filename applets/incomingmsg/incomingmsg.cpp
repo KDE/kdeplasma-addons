c1c0127f2c230600736b053ffbe39cf1f49919f6
Christian Weilbach <christian_weilbach@web.de>
2011-10-27 19:31:53 +0200

    Fix licensing to GPLv2 or later in sources, too.

--- applets/incomingmsg/incomingmsg.cpp
+++ applets/incomingmsg/incomingmsg.cpp
@@ -2,9 +2,10 @@
  *   Copyright (C) 2008-2010 Christian Weilbach <dunsens@web.de>
  *   Copyright (C) 2010 Ruslan Nigmatullin <euroelessar@ya.ru>
  *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU Library General Public License version 2 as
- *   published by the Free Software Foundation
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License as
+ *   published by the Free Software Foundation; either version 2 of
+ *   the License, or (at your option) any later version.
  *
  *   This program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
