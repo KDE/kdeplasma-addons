3acf1421a526810a1dfe20b0bcbdf361c93c1e55
Sebastian Kügler <sebas@kde.org>
2012-01-19 05:34:41 +0100

    add User:<screen_name>@serviceUrl source
    This reflects the users/show/<user>.xml API call, and is there to
    display information about a certain user. Needs a bit of cleanup, but
    basically works.
    
    Not all keys are supported yet.

---
+++ dataengines/microblog/usersource.h
@@ -0,0 +1,74 @@
+/*
+ *   Copyright 2008 Aaron Seigo <aseigo@kde.org>
+ *   Copyright 2012 Sebastian Kügler <sebas@kde.org>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Library General Public License as
+ *   published by the Free Software Foundation; either version 2 or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef USERSOURCE_H
+#define USERSOURCE_H
+
+#include <QList>
+#include <QPair>
+#include <QXmlStreamReader>
+
+#include <KUrl>
+
+#include <Plasma/DataContainer>
+
+// forward declarations
+class KJob;
+
+namespace KIO
+{
+    class Job;
+} // namespace KIO
+
+class UserSource : public Plasma::DataContainer
+{
+Q_OBJECT
+
+public:
+    UserSource(const QString &who,  const QString &serviceBaseUrl, QObject* parent);
+    ~UserSource();
+
+    void loadUserInfo(const QString &who, const KUrl &url);
+    void loadStarted();
+    void loadFinished();
+    Plasma::DataEngine::Data data();
+
+Q_SIGNALS:
+    void dataChanged();
+
+private Q_SLOTS:
+    void recv(KIO::Job*, const QByteArray& data);
+    void result(KJob*);
+
+private:
+    void parse(QXmlStreamReader &reader);
+    void readUser(QXmlStreamReader &reader);
+    QString m_user;
+    QString m_serviceBaseUrl;
+    QByteArray m_xml;
+    QHash<KJob *, QString> m_jobs;
+    QHash<KJob *, QByteArray> m_jobData;
+    int m_runningJobs;
+    QList<QPair<QString, KUrl> > m_queuedJobs;
+    Plasma::DataEngine::Data m_cachedData;
+};
+
+#endif
+
