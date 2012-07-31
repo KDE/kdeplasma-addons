c63e35f3bcf2275bc25ecc613d32ed5acb7c70a7
Luís Gabriel Lima <lampih@gmail.com>
2012-07-31 18:49:09 -0300

    Fix setAlphaF() bug
    Signed-off-by: Luís Gabriel Lima <lampih@gmail.com>

--- applets/weather/package/contents/ui/Utils.js
+++ applets/weather/package/contents/ui/Utils.js
@@ -2,6 +2,10 @@
 
 function setAlphaF(solidColor, alpha) {
     var solid = String(solidColor);
-    var alphaDecimal = Math.round(alpha * 255);
-    return "#" + alphaDecimal.toString(16) + solid.slice(1);
+    var alphaHex = Math.round(alpha * 255).toString(16);
+
+    if (alphaHex.length === 1)
+        alphaHex = "0" + alphaHex;
+
+    return "#" + alphaHex + solid.slice(1);
 }
