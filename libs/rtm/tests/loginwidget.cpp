5e79ad117b605c3f8fadf9900752237e9eee1c5d
Jeremy Whiting <jpwhiting@kde.org>
2012-09-19 14:53:27 -0600

    rtm: Add loginwindow to tests. rtm: Re-enable login test.

--- libs/rtm/tests/loginwidget.cpp
+++ libs/rtm/tests/loginwidget.cpp
@@ -16,30 +16,32 @@
  *   Free Software Foundation, Inc.,
  *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+ 
+#include <KWebView>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QWidget>
 
-#include "session.h"
+class LoginWidget: public QWidget
+{
+  public:
+    LoginWidget(QObject *parent = 0)
+    {
+      webview = new KWebView(this);
+      button = new QPushButton("Press after authenticating", this);
+      
+      QVBoxLayout *layout = new QVBoxLayout(this);
+      layout->addWidget(webview);
+      layout->addWidget(button);
+    }
+    
+    void setWebUrl(const QString &url)
+    {
+      webview->setUrl(url);
+    }
 
-#include "loginwidget.cpp"
-
-#include <QString>
-#include <KApplication>
-#include <KAboutData>
-#include <KCmdLineArgs>
-#include <QtDebug>
-
-int main(int argc, char* argv[]) { 
-  KAboutData about("login", 0, ki18n("RTM Login Test"), "1.0", ki18n("A Simple RTM Login Test"),
-                  KAboutData::License_GPL, ki18n("(C) 2009 Andrew Stromme"), KLocalizedString(), 0, "astromme@chatonka.com");
-  KCmdLineArgs::init(argc, argv, &about);
-  
-  KApplication app;
-  
-  RTM::Session *session = new RTM::Session("myapikey", "mysharedsecret", RTM::Delete, QString(), &app);
-  
-  LoginWidget *login = new LoginWidget();
-  login->setWebUrl(session->getAuthUrl());
-  login->show();
-  
-  return app.exec();
-}
+  private:
+    KWebView *webview;
+    QPushButton *button;
+};
 
