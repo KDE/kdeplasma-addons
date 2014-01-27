bfef26a26be002ffc44b85452881f20119bd2696
Aleix Pol <aleixpol@kde.org>
2014-01-28 00:36:47 +0100

    Use the correct plasmoid name in the Messages.sh file
    CCMAIL: lueck@hube-lueck.de

--- applets/quickshare/Messages.sh
+++ applets/quickshare/Messages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 $EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.cpp *.h -o $podir/plasma_applet_quickshare.pot
+$XGETTEXT *.cpp *.h -o $podir/plasma_applet_org.kde.plasma.quickshare.pot
 rm -f rc.cpp
