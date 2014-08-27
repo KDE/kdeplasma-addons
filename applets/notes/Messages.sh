b8999d832dea7775057d0a5bffc76928bba8d0d8
Albert Astals Cid <aacid@kde.org>
2014-08-27 22:49:31 +0200

    Fix Messages.sh
    CCMAIL: kde@davidedmundson.co.uk

--- applets/notes/Messages.sh
+++ applets/notes/Messages.sh
@@ -1,3 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTRC *.ui >> rc.cpp
-$XGETTEXT *.cpp -o $podir/plasma_applet_org.kde.plasma.notes.pot
+$XGETTEXT `find . -name \*.qml -o -name \*.cpp` -o $podir/plasma_applet_org.kde.plasma.notes.pot
