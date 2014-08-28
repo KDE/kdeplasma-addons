930c4f54557415138f1cfddbf2fef82299cfb949
David Edmundson <kde@davidedmundson.co.uk>
2014-08-29 00:09:41 +0200

    Add interface to load/save notes from a directory
    Not all features implemented yet

---
+++ applets/notes/plugin/filesystemnotemanager.h
@@ -0,0 +1,42 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright (C) 2014  David Edmundson <david@davidedmundson.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef FILESYSTEMNOTEMANAGER_H
+#define FILESYSTEMNOTEMANAGER_H
+
+#include "abstractnotemanager.h"
+
+#include <QObject>
+#include <QStandardPaths>
+#include <QDir>
+
+class FileSystemNoteManager : public AbstractNoteManager
+{
+public:
+    explicit FileSystemNoteManager();
+    virtual QStringList allNoteIds();
+    virtual Note* loadNote(const QString& id);
+    virtual void deleteNoteResources(const QString& id);
+
+private:
+    QDir m_notesDir;
+};
+
+#endif // FILESYSTEMNOTEMANAGER_H
