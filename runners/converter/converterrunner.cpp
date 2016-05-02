54a37fc80c33c1f76faa4975868d1cc824d38dc2
Kai Uwe Broulik <kde@privat.broulik.de>
2016-05-02 21:11:49 +0200

    [Converter Runner] Try QLocale::toDouble to get input value
    Only if that fails, try QString::toDouble and if even that fails skip.
    This allows the user to enter numbers in a more natural way.
    
    REVIEW: 127802

--- runners/converter/converterrunner.cpp
+++ runners/converter/converterrunner.cpp
@@ -227,12 +227,27 @@ void ConverterRunner::match(Plasma::RunnerContext &context)
         }
     }
 
+    QLocale locale;
     KUnitConversion::Unit u1 = category.unit(unit1);
     foreach (const KUnitConversion::Unit& u, units) {
         if (u1 == u) {
             continue;
         }
-        KUnitConversion::Value v = category.convert(KUnitConversion::Value(value.toDouble(), u1), u);
+
+        bool ok;
+        double numberValue = locale.toDouble(value, &ok);
+        if (!ok) {
+            numberValue = value.toDouble(&ok);
+            if (!ok) {
+                continue;
+            }
+        }
+        KUnitConversion::Value v = category.convert(KUnitConversion::Value(numberValue, u1), u);
+
+	if (!v.isValid()) {
+		continue;
+	}
+
         Plasma::QueryMatch match(this);
         match.setType(Plasma::QueryMatch::InformationalMatch);
         match.setIcon(QIcon::fromTheme(QLatin1String( "edit-copy" )));
