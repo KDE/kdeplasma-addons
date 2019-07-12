e0b126692a87b719a8e9bd7b3ab9c242a61d5689
Guo Yunhe <i@guoyunhe.me>
2019-07-12 22:06:46 +0300

    Add Unsplash POTD Provider
    Summary:
    Add Unsplash Wallpaper as picture of the day provider. Unsplash provides high quality photos under public domain license.
    
    {F7013045}
    
    Reviewers: #plasma, ngraham
    
    Reviewed By: ngraham
    
    Subscribers: ngraham, filipf, plasma-devel
    
    Tags: #plasma
    
    Differential Revision: https://phabricator.kde.org/D22426

---
+++ dataengines/potd/unsplashprovider.cpp
@@ -0,0 +1,83 @@
+/*
+ *   Copyright 2019 Guo Yunhe <i@guoyunhe.me>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "unsplashprovider.h"
+
+#include <QDebug>
+#include <QRegularExpression>
+
+#include <KPluginFactory>
+#include <KIO/Job>
+
+UnsplashProvider::UnsplashProvider(QObject* parent, const QVariantList& args)
+    : PotdProvider(parent, args)
+{
+    const QUrl url(QStringLiteral("https://unsplash.com/wallpaper/1065396/desktop-wallpapers"));
+
+    KIO::StoredTransferJob* job = KIO::storedGet(url, KIO::NoReload, KIO::HideProgressInfo);
+    connect(job, &KIO::StoredTransferJob::finished, this, &UnsplashProvider::pageRequestFinished);
+}
+
+UnsplashProvider::~UnsplashProvider() = default;
+
+QImage UnsplashProvider::image() const
+{
+    return mImage;
+}
+
+void UnsplashProvider::pageRequestFinished(KJob* _job)
+{
+    KIO::StoredTransferJob* job = static_cast<KIO::StoredTransferJob*>(_job);
+    if (job->error()) {
+        emit error(this);
+        return;
+    }
+
+    const QString html = QString::fromUtf8(job->data());
+    
+    QRegularExpression re(QStringLiteral("src=\"(https://images\\.unsplash\\.com/photo-\\w+-\\w+)"));
+    
+    QRegularExpressionMatch match = re.match(html);
+    
+    if (match.hasMatch()) {
+        QUrl picUrl(match.captured(1)); // url to full size photo (compressed)
+        KIO::StoredTransferJob* imageJob = KIO::storedGet(picUrl, KIO::NoReload, KIO::HideProgressInfo);
+        connect(imageJob, &KIO::StoredTransferJob::finished, this, &UnsplashProvider::imageRequestFinished);
+        return;
+    } else {
+        emit error(this);
+        return;
+    }
+}
+
+void UnsplashProvider::imageRequestFinished(KJob* _job)
+{
+    KIO::StoredTransferJob* job = static_cast<KIO::StoredTransferJob*>(_job);
+    if (job->error()) {
+        emit error(this);
+        return;
+    }
+    QByteArray data = job->data();
+    mImage = QImage::fromData(data);
+    emit finished(this);
+}
+
+K_PLUGIN_CLASS_WITH_JSON(UnsplashProvider, "unsplashprovider.json")
+
+#include "unsplashprovider.moc"
---
+++ dataengines/potd/unsplashprovider.h
@@ -0,0 +1,67 @@
+/*
+ *   Copyright 2019 Guo Yunhe <i@guoyunhe.me>
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details
+ *
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this program; if not, write to the
+ *   Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef UNSPLASHPROVIDER_H
+#define UNSPLASHPROVIDER_H
+
+#include "potdprovider.h"
+// Qt
+#include <QImage>
+
+class KJob;
+
+/**
+ * This class provides random wallpapers from Unsplash Wallpapers
+ * Image urls are parsed from https://unsplash.com/wallpaper/1065396/desktop-wallpapers
+ */
+class UnsplashProvider : public PotdProvider
+{
+    Q_OBJECT
+
+    public:
+        /**
+         * Creates a new Unsplash provider.
+         *
+         * @param parent The parent object.
+         * @param args The arguments.
+         */
+        UnsplashProvider( QObject *parent, const QVariantList &args );
+
+        /**
+         * Destroys the Unsplash provider.
+         */
+        ~UnsplashProvider() override;
+
+        /**
+         * Returns the requested image.
+         *
+         * Note: This method returns only a valid image after the
+         *       finished() signal has been emitted.
+         */
+        QImage image() const override;
+
+    private:
+        void pageRequestFinished(KJob *job);
+        void imageRequestFinished(KJob *job);
+
+    private:
+        QImage mImage;
+};
+
+#endif
