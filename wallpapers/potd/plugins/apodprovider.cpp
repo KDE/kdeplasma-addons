84525a920b21be8d3823f71e410463d95bcc8726
Fushan Wen <qydwhotmail@gmail.com>
2022-01-22 15:44:26 +0800

    dataengines/potd: Parse title, author, infoUrl and remoteUrl in ApodProvider
    Parse the title and the author from the HTML source, and set the
    info URL and the remote URL.
    
    CCBUG: 422934

--- wallpapers/potd/plugins/apodprovider.cpp
+++ wallpapers/potd/plugins/apodprovider.cpp
@@ -8,7 +8,8 @@
 #include "apodprovider.h"
 
 #include <QDebug>
-#include <QRegExp>
+#include <QRegularExpression>
+#include <QTextDocumentFragment>
 
 #include <KIO/Job>
 #include <KPluginFactory>
@@ -16,9 +17,9 @@
 ApodProvider::ApodProvider(QObject *parent, const KPluginMetaData &data, const QVariantList &args)
     : PotdProvider(parent, data, args)
 {
-    const QUrl url(QStringLiteral("http://antwrp.gsfc.nasa.gov/apod/"));
+    potdProviderData()->wallpaperInfoUrl = QUrl(QStringLiteral("http://antwrp.gsfc.nasa.gov/apod/"));
 
-    KIO::StoredTransferJob *job = KIO::storedGet(url, KIO::NoReload, KIO::HideProgressInfo);
+    KIO::StoredTransferJob *job = KIO::storedGet(potdProviderData()->wallpaperInfoUrl, KIO::NoReload, KIO::HideProgressInfo);
     connect(job, &KIO::StoredTransferJob::finished, this, &ApodProvider::pageRequestFinished);
 }
 
@@ -32,15 +33,34 @@ void ApodProvider::pageRequestFinished(KJob *_job)
         return;
     }
 
-    const QString data = QString::fromUtf8(job->data());
+    const QString data = QString::fromUtf8(job->data()).simplified(); // Join lines so title match can work
 
     const QString pattern = QStringLiteral("<a href=\"(image/.*)\"");
     QRegExp exp(pattern);
     exp.setMinimal(true);
     if (exp.indexIn(data) != -1) {
         const QString sub = exp.cap(1);
-        const QUrl url(QLatin1String("http://antwrp.gsfc.nasa.gov/apod/") + sub);
-        KIO::StoredTransferJob *imageJob = KIO::storedGet(url, KIO::NoReload, KIO::HideProgressInfo);
+        potdProviderData()->wallpaperRemoteUrl = QUrl(QStringLiteral("http://antwrp.gsfc.nasa.gov/apod/") + sub);
+
+        /**
+         * Match title and author
+         * Example:
+         * <b> The Full Moon and the Dancer </b> <br>
+         *
+         * <b>Image Credit &
+         * <a href="lib/about_apod.html#srapply">Copyright</a>:</b>
+         *
+         * <a href="https://www.instagram.com/through_my_lens_84/">Elena Pinna</a>
+         */
+        const QRegularExpression infoRegEx(QStringLiteral("<center>.*?<b>(.+?)</b>.*?Credit.*?:.*?</b>(.+?)</center>"));
+        const QRegularExpressionMatch match = infoRegEx.match(data);
+
+        if (match.hasMatch()) {
+            potdProviderData()->wallpaperTitle = QTextDocumentFragment::fromHtml(match.captured(1).trimmed()).toPlainText();
+            potdProviderData()->wallpaperAuthor = QTextDocumentFragment::fromHtml(match.captured(2).trimmed()).toPlainText();
+        }
+
+        KIO::StoredTransferJob *imageJob = KIO::storedGet(potdProviderData()->wallpaperRemoteUrl, KIO::NoReload, KIO::HideProgressInfo);
         connect(imageJob, &KIO::StoredTransferJob::finished, this, &ApodProvider::imageRequestFinished);
     } else {
         Q_EMIT error(this);
