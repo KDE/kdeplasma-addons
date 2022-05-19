Sonic-DE/sonic-workspace-addons

alternatecalendar: Reload config on config changed
CCBUG: 429892

plasmacalendarplugins/alternatecalendar/alternatecalendarplugin.cpp
@@ -19,6 +19,8 @@ AlternateCalendarPluginPrivate::AlternateCalendarPluginPrivate(AlternateCalendar
 {
     auto config = KSharedConfig::openConfig(QStringLiteral("plasma_calendar_alternatecalendar"));
     m_generalConfigGroup = config->group("General");
+    m_configWatcher = KConfigWatcher::create(config);
+    QObject::connect(m_configWatcher.get(), &KConfigWatcher::configChanged, p, &AlternateCalendarPlugin::updateSettings);
     init();
 }
 
@@ -69,6 +71,9 @@ AlternateCalendarPlugin::AlternateCalendarPlugin()
 
 void AlternateCalendarPlugin::loadEventsForDateRange(const QDate &startDate, const QDate &endDate)
 {
+    m_lastStartDate = startDate;
+    m_lastEndDate = endDate;
+
     QHash<QDate, QDate> alternateDatesData;
     QHash<QDate, SubLabel> subLabelsData;
 
@@ -90,6 +95,12 @@ void AlternateCalendarPlugin::loadEventsForDateRange(const QDate &startDate, con
         Q_EMIT alternateDateReady(alternateDatesData);
     }
     Q_EMIT subLabelReady(subLabelsData);
+}
+
+void AlternateCalendarPlugin::updateSettings()
+{
+    d->init();
+    loadEventsForDateRange(m_lastStartDate, m_lastEndDate);
 }
 
 }

KDE/kdeplasma-addons

plasmacalendarplugins/alternatecalendar/alternatecalendarplugin.cpp
@@ -7,6 +7,7 @@
 #include "alternatecalendarplugin.h"
 
 #include <KConfigGroup>
+#include <KConfigWatcher>
 #include <KSharedConfig>
 
 #include "provider/qtcalendar.h"
@@ -26,7 +27,10 @@ public:
 private:
     std::unique_ptr<AbstractCalendarProvider> m_calendarProvider;
 
+    // For updating config
     KConfigGroup m_generalConfigGroup;
+    KConfigWatcher::Ptr m_configWatcher;
+
     AlternateCalendarPlugin *q;
 };
 
@@ -35,6 +39,8 @@ AlternateCalendarPluginPrivate::AlternateCalendarPluginPrivate(AlternateCalendar
 {
     auto config = KSharedConfig::openConfig(QStringLiteral("plasma_calendar_alternatecalendar"));
     m_generalConfigGroup = config->group("General");
+    m_configWatcher = KConfigWatcher::create(config);
+    QObject::connect(m_configWatcher.get(), &KConfigWatcher::configChanged, q, &AlternateCalendarPlugin::updateSettings);
     init();
 }
 
@@ -87,6 +93,9 @@ void AlternateCalendarPlugin::loadEventsForDateRange(const QDate &startDate, con
         return;
     }
 
+    m_lastStartDate = startDate;
+    m_lastEndDate = endDate;
+
     QHash<QDate, QDate> alternateDatesData;
     QHash<QDate, SubLabel> subLabelsData;
 
@@ -104,4 +113,10 @@ void AlternateCalendarPlugin::loadEventsForDateRange(const QDate &startDate, con
         Q_EMIT alternateDateReady(alternateDatesData);
     }
     Q_EMIT subLabelReady(subLabelsData);
+}
+
+void AlternateCalendarPlugin::updateSettings()
+{
+    d->init();
+    loadEventsForDateRange(m_lastStartDate, m_lastEndDate);
 }