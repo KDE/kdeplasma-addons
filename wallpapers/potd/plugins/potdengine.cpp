Sonic-DE/sonic-workspace-addons

wallpapers/potd: fix multimonitor support
After dataengine was ported away, it requires to use a self-maintained
engine to properly support multi-screen wallpapers.

3 new classes are added:

- PotdEngine: a class to manage clients, create and delete clients on
demand.
- Client: a class to process wallpaper data
- Backend: a class used in QML side, to relay information from the client

The major part of PotdProviderModel are moved to the 3 classes, so the
model class is now a pure model.

BUG: 454333
FIXED-IN: 5.26

wallpapers/potd/plugins/potdengine.cpp
@@ -1,0 +1,260 @@
+/*
+    SPDX-FileCopyrightText: 2022 Fushan Wen <qydwhotmail@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "potdengine.h"
+
+#include <chrono>
+
+#include <QDBusConnection>
+#include <QThreadPool>
+
+#include <KPluginFactory>
+
+#include "cachedprovider.h"
+#include "debug.h"
+
+using namespace std::chrono_literals;
+
+Client::Client(const KPluginMetaData &metadata, const QVariantList &args, QObject *parent)
+    : QObject(parent)
+    , m_metadata(metadata)
+    , m_identifier(metadata.value(QStringLiteral("X-KDE-PlasmaPoTDProvider-Identifier")))
+    , m_args(args)
+{
+    updateSource();
+}
+
+void Client::updateSource(bool refresh)
+{
+    setLoading(true);
+
+    // Check whether it is cached already...
+    if (!refresh && CachedProvider::isCached(m_identifier, m_args, false)) {
+        CachedProvider *provider = new CachedProvider(m_identifier, m_args, this);
+        connect(provider, &PotdProvider::finished, this, &Client::slotFinished);
+        connect(provider, &PotdProvider::error, this, &Client::slotError);
+        return;
+    }
+
+    const auto pluginResult = KPluginFactory::instantiatePlugin<PotdProvider>(m_metadata, this, m_args);
+
+    if (pluginResult) {
+        connect(pluginResult.plugin, &PotdProvider::finished, this, &Client::slotFinished);
+        connect(pluginResult.plugin, &PotdProvider::error, this, &Client::slotError);
+    } else {
+        qCWarning(WALLPAPERPOTD) << "Error loading PoTD plugin:" << pluginResult.errorString;
+    }
+}
+
+void Client::slotFinished(PotdProvider *provider)
+{
+    setImage(provider->image());
+    setInfoUrl(provider->infoUrl());
+    setRemoteUrl(provider->remoteUrl());
+    setTitle(provider->title());
+    setAuthor(provider->author());
+
+    // Store in cache if it's not the response of a CachedProvider
+    if (qobject_cast<CachedProvider *>(provider) == nullptr) {
+        SaveImageThread *thread = new SaveImageThread(m_identifier, m_args, m_data);
+        connect(thread, &SaveImageThread::done, this, &Client::slotCachingFinished);
+        QThreadPool::globalInstance()->start(thread);
+    } else {
+        // Is cache provider
+        setLocalUrl(CachedProvider::identifierToPath(m_identifier, m_args));
+    }
+
+    provider->deleteLater();
+    setLoading(false);
+    Q_EMIT done();
+}
+
+void Client::slotError(PotdProvider *provider)
+{
+    provider->deleteLater();
+    setLoading(false);
+    Q_EMIT done();
+}
+
+void Client::slotCachingFinished(const QString &, const PotdProviderData &data)
+{
+    setLocalUrl(data.wallpaperLocalUrl);
+}
+
+void Client::setImage(const QImage &image)
+{
+    m_data.wallpaperImage = image;
+    Q_EMIT imageChanged();
+}
+
+void Client::setLoading(bool status)
+{
+    m_loading = status;
+    Q_EMIT loadingChanged();
+}
+
+void Client::setLocalUrl(const QString &urlString)
+{
+    m_data.wallpaperLocalUrl = urlString;
+    Q_EMIT localUrlChanged();
+}
+
+void Client::setInfoUrl(const QUrl &url)
+{
+    m_data.wallpaperInfoUrl = url;
+    Q_EMIT infoUrlChanged();
+}
+
+void Client::setRemoteUrl(const QUrl &url)
+{
+    m_data.wallpaperRemoteUrl = url;
+    Q_EMIT remoteUrlChanged();
+}
+
+void Client::setTitle(const QString &title)
+{
+    m_data.wallpaperTitle = title;
+    Q_EMIT titleChanged();
+}
+
+void Client::setAuthor(const QString &author)
+{
+    m_data.wallpaperAuthor = author;
+    Q_EMIT authorChanged();
+}
+
+PotdEngine::PotdEngine(QObject *parent)
+    : QObject(parent)
+    , m_lastUpdateDate(QDate::currentDate().addDays(-1))
+{
+    loadPluginMetaData();
+
+    connect(&m_checkDatesTimer, &QTimer::timeout, this, &PotdEngine::forceUpdateSource);
+    m_checkDatesTimer.setInterval(10min); // check every 10 minutes
+    m_checkDatesTimer.start();
+
+    // Sleep checker
+    QDBusConnection::systemBus().connect(QStringLiteral("org.freedesktop.login1"),
+                                         QStringLiteral("/org/freedesktop/login1"),
+                                         QStringLiteral("org.freedesktop.login1.Manager"),
+                                         QStringLiteral("PrepareForSleep"),
+                                         this,
+                                         SLOT(slotPrepareForSleep(bool)));
+}
+
+Client *PotdEngine::registerClient(const QString &identifier, const QVariantList &args)
+{
+    auto pr = m_clientMap.equal_range(identifier);
+
+    auto createClient = [this, &identifier, &args]() -> Client * {
+        auto pluginIt = m_providersMap.find(identifier);
+
+        if (pluginIt == m_providersMap.end()) {
+            // Not a valid identifier
+            return nullptr;
+        }
+
+        auto client = new Client(pluginIt->second, args, this);
+        m_clientMap.emplace(identifier, ClientPair{client, 1});
+
+        return client;
+    };
+
+    if (pr.first == pr.second) {
+        return createClient();
+    }
+
+    while (pr.first != pr.second) {
+        // find exact match
+        if (pr.first->second.client->m_args == args) {
+            pr.first->second.instanceCount++;
+            return pr.first->second.client;
+        }
+
+        pr.first++;
+    }
+
+    return createClient();
+}
+
+void PotdEngine::unregisterClient(const QString &identifier, const QVariantList &args)
+{
+    auto pr = m_clientMap.equal_range(identifier);
+
+    while (pr.first != pr.second) {
+        // find exact match
+        if (pr.first->second.client->m_args == args) {
+            if (!--pr.first->second.instanceCount) {
+                delete pr.first->second.client;
+                m_clientMap.erase(pr.first);
+                break;
+            }
+        }
+
+        pr.first++;
+    }
+}
+
+void PotdEngine::updateSource(bool refresh)
+{
+    for (const auto &pr : std::as_const(m_clientMap)) {
+        connect(pr.second.client, &Client::done, this, &PotdEngine::slotDone);
+        pr.second.client->updateSource(refresh);
+        m_updateCount++;
+    }
+}
+
+void PotdEngine::forceUpdateSource()
+{
+    updateSource(true);
+}
+
+void PotdEngine::slotDone()
+{
+    disconnect(static_cast<Client *>(sender()), &Client::done, this, &PotdEngine::slotDone);
+
+    if (!--m_updateCount) {
+        // Do not update until next day, and delay 1s to make sure last modified condition is satisfied.
+        m_lastUpdateDate = QDate::currentDate();
+        m_checkDatesTimer.setInterval(QDateTime::currentDateTime().msecsTo(m_lastUpdateDate.startOfDay().addDays(1)) + 1000);
+        m_checkDatesTimer.start();
+    }
+}
+
+void PotdEngine::slotPrepareForSleep(bool sleep)
+{
+    if (sleep) {
+        return;
+    }
+
+    // Resume from sleep
+    if (m_lastUpdateDate != QDate::currentDate()) {
+        // New day new wallpaper
+        forceUpdateSource();
+    } else {
+        // Align the update timer's interval, and delay 1s to make sure last modified condition is satisfied.
+        const int remainingTime = QDateTime::currentDateTime().msecsTo(m_lastUpdateDate.addDays(1).startOfDay()) + 1000;
+
+        // In case the remaining time is too short, set the interval to 1min
+        m_checkDatesTimer.setInterval(std::max(remainingTime, 60 * 1000));
+        m_checkDatesTimer.start();
+    }
+}
+
+void PotdEngine::loadPluginMetaData()
+{
+    const auto plugins = KPluginMetaData::findPlugins(QStringLiteral("potd"));
+
+    m_providersMap.clear();
+    m_providersMap.reserve(plugins.size());
+
+    for (const KPluginMetaData &metadata : plugins) {
+        const QString identifier = metadata.value(QStringLiteral("X-KDE-PlasmaPoTDProvider-Identifier"));
+        if (!identifier.isEmpty()) {
+            m_providersMap.emplace(identifier, metadata);
+        }
+    }
+}

wallpapers/potd/plugins/potdengine.h
@@ -1,0 +1,106 @@
+/*
+    SPDX-FileCopyrightText: 2022 Fushan Wen <qydwhotmail@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <unordered_map>
+
+#include <QDate>
+#include <QObject>
+#include <QTimer>
+
+#include <KPluginMetaData>
+
+#include "potdprovider.h"
+
+/**
+ * This class provides data for the specific identifier and arguments
+ */
+class Client : public QObject
+{
+    Q_OBJECT
+
+public:
+    Client(const KPluginMetaData &metadata, const QVariantList &args, QObject *parent = nullptr);
+
+    void updateSource(bool refresh = false);
+
+    KPluginMetaData m_metadata;
+    PotdProviderData m_data;
+    bool m_loading = false;
+
+Q_SIGNALS:
+    void imageChanged();
+    void loadingChanged();
+    void localUrlChanged();
+    void infoUrlChanged();
+    void remoteUrlChanged();
+    void titleChanged();
+    void authorChanged();
+    void done();
+
+private Q_SLOTS:
+    void slotFinished(PotdProvider *provider);
+    void slotError(PotdProvider *provider);
+    void slotCachingFinished(const QString &source, const PotdProviderData &data);
+
+private:
+    void setImage(const QImage &image);
+    void setLoading(bool status);
+    void setLocalUrl(const QString &urlString);
+    void setInfoUrl(const QUrl &url);
+    void setRemoteUrl(const QUrl &url);
+    void setTitle(const QString &title);
+    void setAuthor(const QString &author);
+
+    QString m_identifier;
+    QVariantList m_args;
+
+    friend class PotdEngine;
+};
+
+/**
+ * This class manages the clients for the Pictures of The Day plugin
+ */
+class PotdEngine : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit PotdEngine(QObject *parent = nullptr);
+
+    /**
+     * Registers the @p identifier in the engine
+     *
+     * A client will be created, and will be automatically destroyed when no backend has
+     * the identifier.
+     *
+     * @return the client that relays signals for the specific identifier
+     */
+    Client *registerClient(const QString &identifier, const QVariantList &args);
+    void unregisterClient(const QString &identifier, const QVariantList &args);
+
+    void updateSource(bool refresh);
+
+private Q_SLOTS:
+    void forceUpdateSource();
+    void slotDone();
+    void slotPrepareForSleep(bool sleep);
+
+private:
+    void loadPluginMetaData();
+
+    struct ClientPair {
+        Client *const client = nullptr;
+        int instanceCount = 0;
+    };
+    std::unordered_multimap<QString /* identifier */, ClientPair> m_clientMap;
+    std::unordered_map<QString, KPluginMetaData> m_providersMap;
+
+    QTimer m_checkDatesTimer;
+    QDate m_lastUpdateDate;
+    int m_updateCount = 0;
+};

wallpapers/potd/plugins/potdplugin.cpp
@@ -4,22 +4,13 @@
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 
-#include "potdprovidermodel.h"
 #include <QQmlContext>
 #include <QQmlEngine>
 #include <QQmlExtensionPlugin>
 
-Q_GLOBAL_STATIC(PotdProviderModel, potdProviderModelSelf)
-
-PotdProviderModel *self(QQmlEngine *engine, QJSEngine *scriptEngine)
-{
-    Q_UNUSED(engine)
-    Q_UNUSED(scriptEngine)
-
-    QQmlEngine::setObjectOwnership(potdProviderModelSelf, QQmlEngine::CppOwnership);
-
-    return potdProviderModelSelf;
-}
+#include "backend.h"
+#include "potdprovider.h"
+#include "potdprovidermodel.h"
 
 class PotdPlugin : public QQmlExtensionPlugin
 {
@@ -33,9 +24,10 @@ public:
 
         qRegisterMetaType<PotdProviderData>();
 
+        qmlRegisterType<Backend>(uri, 1, 0, "Backend");
         qmlRegisterType<PotdProviderModel>(uri, 1, 0, "PotdProviderModel");
-        qmlRegisterSingletonType<PotdProviderModel>(uri, 1, 0, "PotdProviderModelInstance", self);
-        qmlRegisterUncreatableType<PotdProviderModel>(uri, 1, 0, "Global", QStringLiteral("Error: only enums"));
+
+        qmlRegisterUncreatableType<Backend>(uri, 1, 0, "Global", QStringLiteral("Error: only enums"));
     }
 };
 

KDE/kdeplasma-addons

wallpapers/potd/plugins/potdengine.cpp
@@ -1,0 +1,286 @@
+/*
+    SPDX-FileCopyrightText: 2022 Fushan Wen <qydwhotmail@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "potdengine.h"
+
+#include <chrono>
+
+#include <QDBusConnection>
+#include <QThreadPool>
+
+#include <KPluginFactory>
+
+#include "cachedprovider.h"
+#include "debug.h"
+
+using namespace std::chrono_literals;
+
+PotdClient::PotdClient(const KPluginMetaData &metadata, const QVariantList &args, QObject *parent)
+    : QObject(parent)
+    , m_metadata(metadata)
+    , m_identifier(metadata.value(QStringLiteral("X-KDE-PlasmaPoTDProvider-Identifier")))
+    , m_args(args)
+{
+    updateSource();
+}
+
+void PotdClient::updateSource(bool refresh)
+{
+    setLoading(true);
+
+    // Check whether it is cached already...
+    if (!refresh && CachedProvider::isCached(m_identifier, m_args, false)) {
+        CachedProvider *provider = new CachedProvider(m_identifier, m_args, this);
+        connect(provider, &PotdProvider::finished, this, &PotdClient::slotFinished);
+        connect(provider, &PotdProvider::error, this, &PotdClient::slotError);
+        return;
+    }
+
+    const auto pluginResult = KPluginFactory::instantiatePlugin<PotdProvider>(m_metadata, this, m_args);
+
+    if (pluginResult) {
+        connect(pluginResult.plugin, &PotdProvider::finished, this, &PotdClient::slotFinished);
+        connect(pluginResult.plugin, &PotdProvider::error, this, &PotdClient::slotError);
+    } else {
+        qCWarning(WALLPAPERPOTD) << "Error loading PoTD plugin:" << pluginResult.errorString;
+    }
+}
+
+void PotdClient::slotFinished(PotdProvider *provider)
+{
+    setImage(provider->image());
+    setInfoUrl(provider->infoUrl());
+    setRemoteUrl(provider->remoteUrl());
+    setTitle(provider->title());
+    setAuthor(provider->author());
+
+    // Store in cache if it's not the response of a CachedProvider
+    if (qobject_cast<CachedProvider *>(provider) == nullptr) {
+        SaveImageThread *thread = new SaveImageThread(m_identifier, m_args, m_data);
+        connect(thread, &SaveImageThread::done, this, &PotdClient::slotCachingFinished);
+        QThreadPool::globalInstance()->start(thread);
+    } else {
+        // Is cache provider
+        setLocalUrl(CachedProvider::identifierToPath(m_identifier, m_args));
+    }
+
+    provider->deleteLater();
+    setLoading(false);
+    Q_EMIT done(this);
+}
+
+void PotdClient::slotError(PotdProvider *provider)
+{
+    provider->deleteLater();
+    setLoading(false);
+    Q_EMIT done(this);
+}
+
+void PotdClient::slotCachingFinished(const QString &, const PotdProviderData &data)
+{
+    setLocalUrl(data.wallpaperLocalUrl);
+}
+
+void PotdClient::setImage(const QImage &image)
+{
+    m_data.wallpaperImage = image;
+    Q_EMIT imageChanged();
+}
+
+void PotdClient::setLoading(bool status)
+{
+    if (status == m_loading) {
+        return;
+    }
+
+    m_loading = status;
+    Q_EMIT loadingChanged();
+}
+
+void PotdClient::setLocalUrl(const QString &urlString)
+{
+    if (m_data.wallpaperLocalUrl == urlString) {
+        return;
+    }
+
+    m_data.wallpaperLocalUrl = urlString;
+    Q_EMIT localUrlChanged();
+}
+
+void PotdClient::setInfoUrl(const QUrl &url)
+{
+    if (m_data.wallpaperInfoUrl == url) {
+        return;
+    }
+
+    m_data.wallpaperInfoUrl = url;
+    Q_EMIT infoUrlChanged();
+}
+
+void PotdClient::setRemoteUrl(const QUrl &url)
+{
+    if (m_data.wallpaperRemoteUrl == url) {
+        return;
+    }
+
+    m_data.wallpaperRemoteUrl = url;
+    Q_EMIT remoteUrlChanged();
+}
+
+void PotdClient::setTitle(const QString &title)
+{
+    if (m_data.wallpaperTitle == title) {
+        return;
+    }
+
+    m_data.wallpaperTitle = title;
+    Q_EMIT titleChanged();
+}
+
+void PotdClient::setAuthor(const QString &author)
+{
+    if (m_data.wallpaperAuthor == author) {
+        return;
+    }
+
+    m_data.wallpaperAuthor = author;
+    Q_EMIT authorChanged();
+}
+
+PotdEngine::PotdEngine(QObject *parent)
+    : QObject(parent)
+    , m_lastUpdateDate(QDate::currentDate().addDays(-1))
+{
+    loadPluginMetaData();
+
+    connect(&m_checkDatesTimer, &QTimer::timeout, this, &PotdEngine::forceUpdateSource);
+
+    int interval = QDateTime::currentDateTime().msecsTo(QDate::currentDate().addDays(1).startOfDay()) + 1000;
+    m_checkDatesTimer.setInterval(std::max(interval, 60 * 1000));
+    m_checkDatesTimer.start();
+
+    // Sleep checker
+    QDBusConnection::systemBus().connect(QStringLiteral("org.freedesktop.login1"),
+                                         QStringLiteral("/org/freedesktop/login1"),
+                                         QStringLiteral("org.freedesktop.login1.Manager"),
+                                         QStringLiteral("PrepareForSleep"),
+                                         this,
+                                         SLOT(slotPrepareForSleep(bool)));
+}
+
+PotdClient *PotdEngine::registerClient(const QString &identifier, const QVariantList &args)
+{
+    auto pr = m_clientMap.equal_range(identifier);
+
+    auto createClient = [this, &identifier, &args]() -> PotdClient * {
+        auto pluginIt = m_providersMap.find(identifier);
+
+        if (pluginIt == m_providersMap.end()) {
+            // Not a valid identifier
+            return nullptr;
+        }
+
+        auto client = new PotdClient(pluginIt->second, args, this);
+        m_clientMap.emplace(identifier, ClientPair{client, 1});
+
+        return client;
+    };
+
+    if (pr.first == pr.second) {
+        return createClient();
+    }
+
+    while (pr.first != pr.second) {
+        // find exact match
+        if (pr.first->second.client->m_args == args) {
+            pr.first->second.instanceCount++;
+            return pr.first->second.client;
+        }
+
+        pr.first++;
+    }
+
+    return createClient();
+}
+
+void PotdEngine::unregisterClient(const QString &identifier, const QVariantList &args)
+{
+    auto pr = m_clientMap.equal_range(identifier);
+
+    while (pr.first != pr.second) {
+        // find exact match
+        if (pr.first->second.client->m_args == args) {
+            if (!--pr.first->second.instanceCount) {
+                delete pr.first->second.client;
+                m_clientMap.erase(pr.first);
+                break;
+            }
+        }
+
+        pr.first++;
+    }
+}
+
+void PotdEngine::updateSource(bool refresh)
+{
+    for (const auto &pr : std::as_const(m_clientMap)) {
+        connect(pr.second.client, &PotdClient::done, this, &PotdEngine::slotDone);
+        pr.second.client->updateSource(refresh);
+        m_updateCount++;
+    }
+}
+
+void PotdEngine::forceUpdateSource()
+{
+    updateSource(true);
+}
+
+void PotdEngine::slotDone(PotdClient *client)
+{
+    disconnect(client, &PotdClient::done, this, &PotdEngine::slotDone);
+
+    if (!--m_updateCount) {
+        // Do not update until next day, and delay 1s to make sure last modified condition is satisfied.
+        m_lastUpdateDate = QDate::currentDate();
+        m_checkDatesTimer.setInterval(QDateTime::currentDateTime().msecsTo(m_lastUpdateDate.startOfDay().addDays(1)) + 1000);
+        m_checkDatesTimer.start();
+    }
+}
+
+void PotdEngine::slotPrepareForSleep(bool sleep)
+{
+    if (sleep) {
+        return;
+    }
+
+    // Resume from sleep
+    if (m_lastUpdateDate != QDate::currentDate()) {
+        // New day new wallpaper
+        forceUpdateSource();
+    } else {
+        // Align the update timer's interval, and delay 1s to make sure last modified condition is satisfied.
+        const int remainingTime = QDateTime::currentDateTime().msecsTo(m_lastUpdateDate.addDays(1).startOfDay()) + 1000;
+
+        // In case the remaining time is too short, set the interval to 1min
+        m_checkDatesTimer.setInterval(std::max(remainingTime, 60 * 1000));
+        m_checkDatesTimer.start();
+    }
+}
+
+void PotdEngine::loadPluginMetaData()
+{
+    const auto plugins = KPluginMetaData::findPlugins(QStringLiteral("potd"));
+
+    m_providersMap.clear();
+    m_providersMap.reserve(plugins.size());
+
+    for (const KPluginMetaData &metadata : plugins) {
+        const QString identifier = metadata.value(QStringLiteral("X-KDE-PlasmaPoTDProvider-Identifier"));
+        if (!identifier.isEmpty()) {
+            m_providersMap.emplace(identifier, metadata);
+        }
+    }
+}

wallpapers/potd/plugins/potdengine.h
@@ -1,0 +1,106 @@
+/*
+    SPDX-FileCopyrightText: 2022 Fushan Wen <qydwhotmail@gmail.com>
+
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <unordered_map>
+
+#include <QDate>
+#include <QObject>
+#include <QTimer>
+
+#include <KPluginMetaData>
+
+#include "potdprovider.h"
+
+/**
+ * This class provides data for the specific identifier and arguments
+ */
+class PotdClient : public QObject
+{
+    Q_OBJECT
+
+public:
+    PotdClient(const KPluginMetaData &metadata, const QVariantList &args, QObject *parent = nullptr);
+
+    void updateSource(bool refresh = false);
+
+    KPluginMetaData m_metadata;
+    PotdProviderData m_data;
+    bool m_loading = false;
+
+Q_SIGNALS:
+    void imageChanged();
+    void loadingChanged();
+    void localUrlChanged();
+    void infoUrlChanged();
+    void remoteUrlChanged();
+    void titleChanged();
+    void authorChanged();
+    void done(PotdClient *client);
+
+private Q_SLOTS:
+    void slotFinished(PotdProvider *provider);
+    void slotError(PotdProvider *provider);
+    void slotCachingFinished(const QString &source, const PotdProviderData &data);
+
+private:
+    void setImage(const QImage &image);
+    void setLoading(bool status);
+    void setLocalUrl(const QString &urlString);
+    void setInfoUrl(const QUrl &url);
+    void setRemoteUrl(const QUrl &url);
+    void setTitle(const QString &title);
+    void setAuthor(const QString &author);
+
+    QString m_identifier;
+    QVariantList m_args;
+
+    friend class PotdEngine;
+};
+
+/**
+ * This class manages the clients for the Pictures of The Day plugin
+ */
+class PotdEngine : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit PotdEngine(QObject *parent = nullptr);
+
+    /**
+     * Registers the @p identifier in the engine
+     *
+     * A client will be created, and will be automatically destroyed when no backend has
+     * the identifier.
+     *
+     * @return the client that relays signals for the specific identifier
+     */
+    PotdClient *registerClient(const QString &identifier, const QVariantList &args);
+    void unregisterClient(const QString &identifier, const QVariantList &args);
+
+    void updateSource(bool refresh);
+
+private Q_SLOTS:
+    void forceUpdateSource();
+    void slotDone(PotdClient *client);
+    void slotPrepareForSleep(bool sleep);
+
+private:
+    void loadPluginMetaData();
+
+    struct ClientPair {
+        PotdClient *const client = nullptr;
+        int instanceCount = 0;
+    };
+    std::unordered_multimap<QString /* identifier */, ClientPair> m_clientMap;
+    std::unordered_map<QString, KPluginMetaData> m_providersMap;
+
+    QTimer m_checkDatesTimer;
+    QDate m_lastUpdateDate;
+    int m_updateCount = 0;
+};

wallpapers/potd/plugins/potdplugin.cpp
@@ -4,22 +4,13 @@
     SPDX-License-Identifier: GPL-2.0-or-later
 */
 
-#include "potdprovidermodel.h"
 #include <QQmlContext>
 #include <QQmlEngine>
 #include <QQmlExtensionPlugin>
 
-Q_GLOBAL_STATIC(PotdProviderModel, potdProviderModelSelf)
-
-PotdProviderModel *self(QQmlEngine *engine, QJSEngine *scriptEngine)
-{
-    Q_UNUSED(engine)
-    Q_UNUSED(scriptEngine)
-
-    QQmlEngine::setObjectOwnership(potdProviderModelSelf, QQmlEngine::CppOwnership);
-
-    return potdProviderModelSelf;
-}
+#include "potdbackend.h"
+#include "potdprovider.h"
+#include "potdprovidermodel.h"
 
 class PotdPlugin : public QQmlExtensionPlugin
 {
@@ -33,9 +24,10 @@ public:
 
         qRegisterMetaType<PotdProviderData>();
 
+        qmlRegisterType<PotdBackend>(uri, 1, 0, "PotdBackend");
         qmlRegisterType<PotdProviderModel>(uri, 1, 0, "PotdProviderModel");
-        qmlRegisterSingletonType<PotdProviderModel>(uri, 1, 0, "PotdProviderModelInstance", self);
-        qmlRegisterUncreatableType<PotdProviderModel>(uri, 1, 0, "Global", QStringLiteral("Error: only enums"));
+
+        qmlRegisterUncreatableType<PotdBackend>(uri, 1, 0, "Global", QStringLiteral("Error: only enums"));
     }
 };
 