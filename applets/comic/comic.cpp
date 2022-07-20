3c60256f1e94397ddccba3151a02d1979a28bace
Nicolas Fella <nicolas.fella@gmx.de>
2022-07-20 19:05:12 +0200

    [applets/comic] Fix configurationRequired and busy state when deactivating the last provider
    Currently when removing the last provider we briefly show a busy indicator and configure button and then remove both
    
    Now only the configure button is shown, permanently

--- applets/comic/comic.cpp
+++ applets/comic/comic.cpp
@@ -187,7 +187,7 @@ void ComicApplet::dataUpdated(const ComicMetaData &data)
         return;
     }
 
-    setConfigurationRequired(false);
+    setConfigurationRequired(mCurrent.id().isEmpty());
 
     // there was an error, display information as image
     if (data.error) {
@@ -503,9 +503,9 @@ void ComicApplet::updateComic(const QString &identifierSuffix)
         mEngine->requestSource(identifier);
         slotScaleToContent();
     } else {
+        setBusy(false);
         qWarning() << "Either no identifier was specified or the engine could not be created:"
                    << "id" << id;
-        setConfigurationRequired(true);
     }
     updateContextMenu();
 }
