c64d9b1701cefe976ce06ea4e4e90e2a9b025e89
Fushan Wen <qydwhotmail@gmail.com>
2022-12-19 01:19:58 +0800

    alternatecalendar: show Persian month instead of Gregorian month
    BUG: 463196
    FIXED-IN: 5.27

--- plasmacalendarplugins/alternatecalendar/provider/qtcalendar.cpp
+++ plasmacalendarplugins/alternatecalendar/provider/qtcalendar.cpp
@@ -16,11 +16,15 @@ public:
     CalendarEvents::CalendarEventsPlugin::SubLabel subLabels(const QDate &date) const;
 
 private:
+    QString persianCalendarSubLabels(const QDate &altDate) const;
+
     const QCalendar m_calendar;
+    const QCalendar::System m_system;
 };
 
 QtCalendarProviderPrivate::QtCalendarProviderPrivate(QCalendar::System system)
     : m_calendar(QCalendar(system))
+    , m_system(system)
 {
 }
 
@@ -47,9 +51,24 @@ CalendarEvents::CalendarEventsPlugin::SubLabel QtCalendarProviderPrivate::subLab
     }
 
     const QDate altDate = fromGregorian(date);
-    sublabel.label = QLocale::system().toString(altDate);
+
+    if (m_system == QCalendar::System::Jalali) {
+        sublabel.label = persianCalendarSubLabels(altDate);
+    } else {
+        sublabel.label = QLocale::system().toString(altDate);
+    }
 
     return sublabel;
+}
+
+QString QtCalendarProviderPrivate::persianCalendarSubLabels(const QDate &altDate) const
+{
+    return i18ndc("plasma_calendar_alternatecalendar",
+                  "%1 Persian day %2 Persian month %3 Persian year",
+                  "%1 %2, %3",
+                  QString::number(altDate.day()),
+                  m_calendar.standaloneMonthName(QLocale::system(), altDate.month()),
+                  QString::number(altDate.year()));
 }
 
 QtCalendarProvider::QtCalendarProvider(QObject *parent, CalendarSystem::System calendarSystem)
