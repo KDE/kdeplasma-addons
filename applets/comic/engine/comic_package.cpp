0005d90d539a8da1c4088c3764f8ce155f6c7faa
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-04-02 06:54:39 +0200

    ComicPackage: Call file/dir definitions without translations
    Depends on https://invent.kde.org/frameworks/kpackage/-/merge_requests/74

--- applets/comic/engine/comic_package.cpp
+++ applets/comic/engine/comic_package.cpp
@@ -17,16 +17,16 @@ ComicPackage::ComicPackage(QObject *parent, const QVariantList &args)
 void ComicPackage::initPackage(KPackage::Package *package)
 {
     QStringList mimetypes;
-    package->addDirectoryDefinition("images", QLatin1String("images"), i18n("Images"));
+    package->addDirectoryDefinition("images", QLatin1String("images"));
     mimetypes << QLatin1String("image/svg+xml") << QLatin1String("image/png") << QLatin1String("image/jpeg");
     package->setMimeTypes("images", mimetypes);
 
     mimetypes.clear();
-    package->addDirectoryDefinition("scripts", QLatin1String("code"), i18n("Executable Scripts"));
+    package->addDirectoryDefinition("scripts", QLatin1String("code"));
     mimetypes << QLatin1String("text/*");
     package->setMimeTypes("scripts", mimetypes);
 
-    package->addFileDefinition("mainscript", QLatin1String("code/main"), i18n("Main Script File"));
+    package->addFileDefinition("mainscript", QLatin1String("code/main"));
     // package->setRequired("mainscript", true); Package::isValid() fails with this because of Kross and different file extensions
     package->setDefaultPackageRoot(QStringLiteral("plasma/comics/"));
 }
