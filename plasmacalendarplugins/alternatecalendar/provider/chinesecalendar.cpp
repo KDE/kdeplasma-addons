d54c7aecd98d560cb0729b1f7b6ff32808771889
Fushan Wen <qydwhotmail@gmail.com>
2023-05-15 21:55:57 +0800

    alternatecalendar: support solar terms in Chinese lunar calendar
    In Chinese lunar calendar, solar terms, also known as "节气", are
    points in time that mark the changing seasons and the solar movements.
    There are 24 solar terms in total, with each term representing a
    specific astronomical event or natural phenomenon. These solar terms
    are based on the position of the sun relative to the Earth and are
    used to determine the timing of various agricultural activities,
    traditional festivals, and other cultural practices in China.

--- plasmacalendarplugins/alternatecalendar/provider/chinesecalendar.cpp
+++ plasmacalendarplugins/alternatecalendar/provider/chinesecalendar.cpp
@@ -7,6 +7,18 @@
 
 #include "chinesecalendar.h"
 
+#include "solarutils.h"
+
+namespace
+{
+const QStringList s_solarTermNames{
+    QStringLiteral("春分"), QStringLiteral("清明"), QStringLiteral("谷雨"), QStringLiteral("立夏"), QStringLiteral("小满"), QStringLiteral("芒种"),
+    QStringLiteral("夏至"), QStringLiteral("小暑"), QStringLiteral("大暑"), QStringLiteral("立秋"), QStringLiteral("处暑"), QStringLiteral("白露"),
+    QStringLiteral("秋分"), QStringLiteral("寒露"), QStringLiteral("霜降"), QStringLiteral("立冬"), QStringLiteral("小雪"), QStringLiteral("大雪"),
+    QStringLiteral("冬至"), QStringLiteral("小寒"), QStringLiteral("大寒"), QStringLiteral("立春"), QStringLiteral("雨水"), QStringLiteral("惊蛰"),
+};
+}
+
 class ChineseCalendarProviderPrivate : public ICUCalendarPrivate
 {
 public:
@@ -15,6 +27,33 @@ public:
     CalendarEvents::CalendarEventsPlugin::SubLabel subLabels(const QDate &date);
 
 private:
+    enum SolarTerm {
+        ChunFen = 0,
+        QingMing,
+        GuYu,
+        LiXia,
+        XiaoMan,
+        MangZhong,
+        XiaZhi,
+        XiaoShu,
+        DaShu,
+        LiQiu,
+        ChuShu,
+        BaiLu,
+        QiuFen,
+        HanLu,
+        ShuangJiang,
+        LiDong,
+        XiaoXue,
+        DaXue,
+        DongZhi,
+        XiaoHan,
+        DaHan,
+        LiChun,
+        YuShui,
+        JingZhe,
+    };
+
     /**
      * For formatting, see the documentation of SimpleDateFormat:
      * https://unicode-org.github.io/icu-docs/apidoc/released/icu4c/classicu_1_1SimpleDateFormat.html#details
@@ -25,9 +64,20 @@ private:
     QString monthDisplayName() const;
     QString dayDisplayName() const;
 
+    auto generateSolarTermsCache(int year);
+
+    /**
+     * Calculates Julian day of a solar term based on Newton's method
+     */
+    QDate getSolarTermDate(int year, SolarTerm order) const;
+
+    static QHash<int /*year*/, std::array<QDate, 25>> s_solarTermsMap;
+
     icu::Locale m_locale;
     icu::Locale m_hanidaysLocale;
 };
+
+QHash<int, std::array<QDate, 25>> ChineseCalendarProviderPrivate::s_solarTermsMap = QHash<int, std::array<QDate, 25>>();
 
 ChineseCalendarProviderPrivate::ChineseCalendarProviderPrivate()
     : ICUCalendarPrivate()
@@ -41,6 +91,28 @@ ChineseCalendarProviderPrivate::ChineseCalendarProviderPrivate()
     m_calendar.reset(icu::Calendar::createInstance("en_US@calendar=chinese", m_errorCode));
 }
 
+auto ChineseCalendarProviderPrivate::generateSolarTermsCache(int year)
+{
+    auto thisYearIt = s_solarTermsMap.find(year);
+    if (thisYearIt != s_solarTermsMap.end()) {
+        // Already generated
+        return thisYearIt;
+    }
+
+    thisYearIt = s_solarTermsMap.insert(year, {});
+
+    SolarTerm solarTermIndex = DongZhi;
+    for (int i = 0; i < 25; i++) {
+        (*thisYearIt)[i] = getSolarTermDate(year - 1, solarTermIndex);
+        if (solarTermIndex == DongZhi) {
+            year++;
+        }
+        solarTermIndex = static_cast<SolarTerm>((solarTermIndex + 1) % 24);
+    }
+
+    return thisYearIt;
+}
+
 CalendarEvents::CalendarEventsPlugin::SubLabel ChineseCalendarProviderPrivate::subLabels(const QDate &date)
 {
     auto sublabel = CalendarEvents::CalendarEventsPlugin::SubLabel{};
@@ -51,9 +123,29 @@ CalendarEvents::CalendarEventsPlugin::SubLabel ChineseCalendarProviderPrivate::s
 
     sublabel.yearLabel = yearDisplayName();
     sublabel.monthLabel = monthDisplayName();
+
+    // Check solar term cache exists
+    auto it = s_solarTermsMap.find(date.year());
+    if (it == s_solarTermsMap.end()) {
+        it = generateSolarTermsCache(date.year());
+    }
+
+    int solarTermIndex = -1;
+    {
+        const int indexInList = 2 * date.month() - 1;
+        const QDate date1 = it->at(indexInList);
+        const QDate date2 = it->at(indexInList + 1);
+        if (date1.day() == date.day()) {
+            solarTermIndex = (indexInList + DongZhi) % 24;
+        } else if (date2.day() == date.day()) {
+            solarTermIndex = (indexInList + 1 + DongZhi) % 24;
+        }
+    }
+
     const QString dayName = dayDisplayName();
-    sublabel.dayLabel = day() == 1 ? monthDisplayName() : dayName;
-    sublabel.label = QStringLiteral("%1%2%3").arg(sublabel.yearLabel, sublabel.monthLabel, dayName);
+    sublabel.dayLabel = day() == 1 ? monthDisplayName() : (solarTermIndex >= 0 ? s_solarTermNames.at(solarTermIndex) : dayName);
+    const QString solarTerm = solarTermIndex >= 0 ? QStringLiteral(" (%1)").arg(s_solarTermNames.at(solarTermIndex)) : QString();
+    sublabel.label = QStringLiteral("%1%2%3%4").arg(sublabel.yearLabel, sublabel.monthLabel, dayName, solarTerm);
     sublabel.priority = CalendarEvents::CalendarEventsPlugin::SubLabelPriority::Low;
 
     return sublabel;
@@ -86,6 +178,31 @@ QString ChineseCalendarProviderPrivate::monthDisplayName() const
 QString ChineseCalendarProviderPrivate::dayDisplayName() const
 {
     return formattedDateString("d", true);
+}
+
+QDate ChineseCalendarProviderPrivate::getSolarTermDate(int year, SolarTerm order) const
+{
+    constexpr double RADIANS_PER_TERM = std::numbers::pi / 12.0;
+    double angle = double(order) * RADIANS_PER_TERM;
+    int month = ((order + 1) / 2 + 2) % 12 + 1;
+    // 春分: Mar 20th
+    int day = 6;
+    if (order % 2 == 0) {
+        day = 20;
+    }
+    const std::int64_t initialJulianDay = SolarUtils::toJulianDay(year, month, day);
+    // Can't use QDate::toJulianDay because it doesn't support extra hours
+    double julianDay = SolarUtils::NewtonIteration(angle, initialJulianDay);
+    // To UTC+8 time
+    julianDay += 8.0 / 24.0;
+
+    int resultYear, resultMonth, resultDay;
+    SolarUtils::getDateFromJulianDay(julianDay, resultYear, resultMonth, resultDay);
+    //  TT -> UTC
+    julianDay -= SolarUtils::getDeltaT(resultYear, resultMonth) / 86400;
+    SolarUtils::getDateFromJulianDay(julianDay, resultYear, resultMonth, resultDay);
+
+    return QDate(resultYear, resultMonth, resultDay);
 }
 
 ChineseCalendarProvider::ChineseCalendarProvider(QObject *parent, CalendarSystem::System calendarSystem)
