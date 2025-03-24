5fc3288c24c9c290c2f5de9637c63ce0581ca965
Marco Martin <notmart@gmail.com>
2025-03-24 12:52:21 +0000

    applets/grouped: Port to nested containments
    The grouping plasmoid is now a properly nested containment
    without the proxy container applet
    
    needs https://invent.kde.org/plasma/libplasma/-/merge_requests/1244

--- applets/grouping/groupingcontainment.cpp
+++ applets/grouping/groupingcontainment.cpp
@@ -9,6 +9,7 @@
 
 #include <QDebug>
 #include <QMenu>
+#include <QQueue>
 #include <QQuickItem>
 #include <QQuickWindow>
 #include <QScreen>
@@ -16,10 +17,78 @@
 
 #include <KActionCollection> // Applet::actions
 
+#include <Plasma/Corona>
+
 GroupingContainment::GroupingContainment(QObject *parent, const KPluginMetaData &data, const QVariantList &args)
     : Plasma::Containment(parent, data, args)
 {
     setHasConfigurationInterface(true);
+}
+
+void GroupingContainment::init()
+{
+    migrateFromContainer();
+}
+
+void GroupingContainment::migrateFromContainer()
+{
+    // Search the old grouping containment containment config group
+    KConfigGroup rootCg(corona()->config(), QStringLiteral("Containments"));
+    // NOTE: this function is called from tyhe constructor, so we can't use config() yet
+    KConfigGroup ownCg = KConfigGroup(config());
+    // old Configuration group of the old grouping containment applet
+    KConfigGroup oldAppletCg(&ownCg, QStringLiteral("Configuration"));
+    const uint oldContId = oldAppletCg.readEntry(QStringLiteral("ContainmentId"), 0);
+
+    if (oldContId == 0) {
+        return;
+    }
+
+    // Config group of the old grouping containment containment
+    KConfigGroup oldContCg = KConfigGroup(&rootCg, QString::number(oldContId));
+
+    // Copy everything all the applets inside, all the subgroup of any applet
+    QQueue<QPair<KConfigGroup, KConfigGroup>> queue;
+    queue.enqueue(qMakePair(oldContCg, ownCg));
+    // Iterative Tree traversal of the grouping containment configuration
+    while (!queue.isEmpty()) {
+        QPair<KConfigGroup, KConfigGroup> current = queue.dequeue();
+        const KConfigGroup &currentSource = current.first;
+        KConfigGroup &currentDest = current.second;
+
+        // Copy all entries in the current group
+        const QMap<QString, QString> entries = currentSource.entryMap();
+        for (auto it = entries.constBegin(); it != entries.constEnd(); ++it) {
+            currentDest.writeEntry(it.key(), currentSource.readEntry(it.key(), QString()));
+        }
+
+        const QStringList groups = currentSource.groupList();
+        for (const QString &group : groups) {
+            KConfigGroup sourceSubGroup = currentSource.group(group);
+            KConfigGroup destSubGroup = currentDest.group(group);
+            queue.enqueue(qMakePair(sourceSubGroup, destSubGroup));
+        }
+    }
+
+    // Delete the the old grouping containment
+    bool oldContainmentInstantiated = false;
+    for (Plasma::Containment *cont : corona()->containments()) {
+        if (cont->id() == oldContId) {
+            delete cont;
+            break;
+        }
+    }
+    // To make sure the old containment is really gone from the config first delete then
+    // remove the group, as destroy() might fail
+    rootCg.deleteGroup(QString::number(oldContId));
+
+    // Delete the old unused key ContainmentId in this grouping containment
+    oldAppletCg.deleteEntry(QStringLiteral("ContainmentId"));
+
+    for (Applet *a : applets()) {
+        a->configChanged();
+    }
+    rootCg.sync();
 }
 
 void GroupingContainment::newTask(const QString &task)
