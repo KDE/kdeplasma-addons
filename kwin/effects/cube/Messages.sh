c677c2790a8200da271b079993dd3128b76fba49
Nate Graham <nate@kde.org>
2025-12-29 10:36:04 -0700

    Fix effects_cube translation
    (cherry picked from commit 36cf02346e9fed30e648f31da51463cefb22a50a)
    
    Co-authored-by: Victor Ryzhykh <victorr2007@yandex.ru>

--- kwin/effects/cube/Messages.sh
+++ kwin/effects/cube/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-$EXTRACTRC *.ui >> rc.cpp || exit 11
+$EXTRACTRC `find -name \*.ui` >> rc.cpp || exit 11
 $XGETTEXT `find . -name \*.cpp -o -name \*.qml` -o $podir/kwin_effect_cube.pot
