b69fd013b138244dd3d0c38812181bf30fc62a8c
Nate Graham <nate@kde.org>
2026-09-03 19:50:51 -0400

    applets/weather: don't process pending events inside ioncontrol_p destructor
    BUG: 524929
    
    
    (cherry picked from commit 415e3a201e845b4f61dded1bd2a74f4e61bddce9)
    
    Co-authored-by: Bohdan Onofriichuk <bogdan.onofriuchuk@gmail.com>

--- applets/weather/ioncontrol_p.cpp
+++ applets/weather/ioncontrol_p.cpp
@@ -61,12 +61,6 @@ IonControl::~IonControl()
         m_fetchThread.reset();
     }
 
-    qCDebug(WEATHER::CONTROLLER) << "IonControl " << m_ionName << " process events";
-
-    // Check for pending events before destroying, as m_fetchThread may still
-    // have queued events that could cause a crash.
-    thread()->eventDispatcher()->processEvents(QEventLoop::AllEvents);
-
     qCDebug(WEATHER::CONTROLLER) << "IonControl " << m_ionName << ": destroyed";
 }
 
